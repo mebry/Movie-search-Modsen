0cc300f60fdb25db6ad3ccd3b8434bd9a5c025e5
Osten Power <osten_power@mail.ru>
2023-06-13 10:15:38 +0300

    add ServiceProviderExtensions

---
+++ src/Services/Rating/Rating.DataAccess/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rating.DataAccess.Contexts;
+using Rating.DataAccess.Repositories.FilmRepositories;
+using Rating.DataAccess.Repositories.RaitingRepositories;
+
+namespace Rating.DataAccess.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddDataAccessService(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddConfigurationMSSQLServer(configuration);
+
+            service.AddRepositoriesService();
+        }
+
+        private static void AddConfigurationMSSQLServer(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Raiting.DataAccess")));
+        }
+
+        private static void AddRepositoriesService(this IServiceCollection service)
+        {
+            service.AddScoped<IFilmRepository, FilmRepository>();
+            service.AddScoped<IRatingFilmRepository, RatingFilmRepository>();
+        }
+    }
+}
