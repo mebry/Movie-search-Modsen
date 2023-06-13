ad544db9b626539801075d981c9a24df73149a89
Osten Power <osten_power@mail.ru>
2023-06-13 15:03:15 +0300

    plug in di

--- src/Services/Rating/Rating.API/Program.cs
+++ src/Services/Rating/Rating.API/Program.cs
@@ -1,8 +1,16 @@
+using AspWebApi.Middlewares;
+using Rating.BusinessLogic.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configure = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddBusinessLogicService(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
