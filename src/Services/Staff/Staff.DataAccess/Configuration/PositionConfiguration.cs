e4f112afdc11f93512509fd99f4b0ec749729b37
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-13 15:49:40 +0300

    add configurations

---
+++ src/Services/Staff/Staff.DataAccess/Configuration/PositionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Staff.DataAccess.Entities;
+
+namespace Staff.DataAccess.Configuration
+{
+    public class PositionConfiguration : IEntityTypeConfiguration<Position>
+    {
+        public void Configure(EntityTypeBuilder<Position> builder)
+        {
+            builder.Property(x => x.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
---
+++ src/Services/Staff/Staff.DataAccess/Configuration/StaffPersonPositionConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Staff.DataAccess.Entities;
+
+namespace Staff.DataAccess.Configuration
+{
+    public class StaffPersonPositionConfiguration : IEntityTypeConfiguration<StaffPersonPosition>
+    {
+        public void Configure(EntityTypeBuilder<StaffPersonPosition> builder)
+        {
+            builder.Property(x => new { x.StaffPersonId, x.PositionId });
+
+            builder
+                .HasOne(x => x.StaffPerson)
+                .WithMany(x => x.StaffPersonPositions)
+                .HasForeignKey(x => x.StaffPersonId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(x => x.Position)
+                .WithMany(x => x.StaffPersonPositions)
+                .HasForeignKey(x => x.PositionId);
+        }
+    }
+}
