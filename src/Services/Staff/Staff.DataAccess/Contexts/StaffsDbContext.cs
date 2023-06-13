6532f6a6c1e741727556ffc6f8dba5b587e9382c
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-13 14:02:21 +0300

    add context

---
+++ src/Services/Staff/Staff.DataAccess/Contexts/StaffsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Staff.DataAccess.Entities;
+
+namespace Staff.DataAccess.Contexts
+{
+    public class StaffsDbContext : DbContext
+    {
+        public DbSet<StaffPerson> Staff { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<StaffPersonPosition> StaffPersonPositions { get; set; }
+
+    }
+}
