91172687e4ae2529c2aecb0d6f9bf6fdf69a7651
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-13 18:02:33 +0300

    add IRepository

---
+++ src/Services/Staff/Staff.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Staff.DataAccess.Repositories.Interfaces
+{
+    public interface IRepository<T>
+    {
+        public Task<ICollection<T>> GetAllAsync();
+        public Task<ICollection<T>> GetAllAsync(Expression<Func<T, bool>> filter);
+        public Task<T> GetAsync(Guid id);
+        public Task<T> GetAsync(Expression<Func<T, bool>> filter);
+        public Task CreateAsync(T entity);
+        public Task RemoveAsync(Guid id);
+        public Task UpdateAsync(T entity);
+    }
+}
