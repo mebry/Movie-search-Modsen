5198f7f1ade766314d3aa6b7e6c438e4a5391495
Osten Power <osten_power@mail.ru>
2023-06-13 14:46:08 +0300

    add controllers

---
+++ src/Services/Rating/Rating.API/Controllers/FilmController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rating.BusinessLogic.DTOs;
+using Rating.BusinessLogic.Services.FilmServices;
+
+namespace Rating.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilmController : ControllerBase
+    {
+        private readonly IFilmService _filmService;
+
+        public FilmController(IFilmService filmService)
+        {
+            _filmService = filmService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FilmDTO>> GetById(Guid id)
+        {
+            var film = await _filmService.GetByIdAsync(id);
+
+            return Ok(film);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FilmDTO>> Create(FilmDTO filmDto)
+        {
+            var film = await _filmService.Create(filmDto);
+
+            return Ok(film);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<FilmDTO>> Update(FilmDTO filmDto)
+        {
+            var film = await _filmService.Update(filmDto);
+
+            return Ok(film);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<FilmDTO>> Delete(Guid id)
+        {
+            var filmDto = new FilmDTO
+            {
+                Id = id
+            };
+            var film = await _filmService.Delete(filmDto);
+
+            return Ok(film);
+        }
+    }
+}
---
+++ src/Services/Rating/Rating.API/Controllers/RatingController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rating.BusinessLogic.DTOs;
+using Rating.BusinessLogic.Services.RatingServices;
+
+namespace Rating.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        private readonly IRatingService _ratingService;
+
+        public RatingController(IRatingService ratingService)
+        {
+            _ratingService = ratingService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RatingDTO>> GetById(Guid id)
+        {
+            var rating = await _ratingService.GetByIdAsync(id);
+
+            return Ok(rating);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<RatingDTO>> Create(RatingDTO ratingDto)
+        {
+            var rating = await _ratingService.Create(ratingDto);
+
+            return Ok(rating);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<RatingDTO>> Update(RatingDTO ratingDto)
+        {
+            var rating = await _ratingService.Update(ratingDto);
+
+            return Ok(rating);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<RatingDTO>> Delete(Guid id)
+        {
+            var ratingDto = new RatingDTO
+            {
+                Id = id
+            };
+            var rating = await _ratingService.Delete(ratingDto);
+
+            return Ok(rating);
+        }
+    }
+}
