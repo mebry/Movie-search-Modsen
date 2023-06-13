6ad0ac51a55ded5909b73eac7f0b79eee8bb6f15
Osten Power <osten_power@mail.ru>
2023-06-13 10:14:35 +0300

    correct ApplicationContext

--- src/Services/Rating/Rating.DataAccess/Contexts/ApplicationContext.cs
+++ src/Services/Rating/Rating.DataAccess/Contexts/ApplicationContext.cs
@@ -6,7 +6,7 @@ namespace Rating.DataAccess.Contexts
     public class ApplicationContext : DbContext
     {
         public DbSet<Film> Films { get; set; }
-        public DbSet<Raiting> Raitings { get; set; }
+        public DbSet<RatingFilm> Ratings { get; set; }
 
         public ApplicationContext(DbContextOptions<ApplicationContext> options)
             : base(options)
