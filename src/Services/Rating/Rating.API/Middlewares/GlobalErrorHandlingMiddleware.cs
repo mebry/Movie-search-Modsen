3453eed4d44a34f3b526fc941caf2f3bd6ccb1c0
Osten Power <osten_power@mail.ru>
2023-06-13 14:46:24 +0300

    add GlobalErrorHandlingMiddleware

---
+++ src/Services/Rating/Rating.API/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,84 @@
+﻿using Rating.BusinessLogic.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+using KeyNotFoundException = Rating.BusinessLogic.Exceptions.KeyNotFoundException;
+using NotImplementedException = Rating.BusinessLogic.Exceptions.NotImplementedException;
+using UnauthorizedAccessException = Rating.BusinessLogic.Exceptions.UnauthorizedAccessException;
+
+namespace AspWebApi.Middlewares
+{
+    public class GlobalErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            HttpStatusCode status;
+            var stackTrace = string.Empty;
+            string message;
+
+            var exceptionType = exception.GetType();
+
+            if(exceptionType == typeof(BadRequestException))
+            {
+                message = exception.Message;
+                status = HttpStatusCode.BadRequest;
+                stackTrace = exception.StackTrace;
+            }
+            else if(exceptionType == typeof(NotFoundException))
+            {
+                message = exception.Message;
+                status = HttpStatusCode.NotFound;
+                stackTrace = exception.StackTrace;
+            }
+            else if(exceptionType == typeof(NotImplementedException))
+            {
+                status = HttpStatusCode.NotImplemented;
+                message = exception.Message;
+                stackTrace = exception.StackTrace;
+            }
+            else if(exceptionType == typeof(UnauthorizedAccessException))
+            {
+                status = HttpStatusCode.Unauthorized;
+                message = exception.Message;
+                stackTrace = exception.StackTrace;
+            }
+            else if(exceptionType == typeof(KeyNotFoundException))
+            {
+                status = HttpStatusCode.Unauthorized;
+                message = exception.Message;
+                stackTrace = exception.StackTrace;
+            }
+            else
+            {
+                status = HttpStatusCode.InternalServerError;
+                message = exception.Message;
+                stackTrace = exception.StackTrace;
+            }
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = message, stackTrace });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+
+            return context.Response.WriteAsync(exceptionResult);
+        }
+    }
+}
