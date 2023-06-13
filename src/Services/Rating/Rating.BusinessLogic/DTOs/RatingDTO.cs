79fec92527dacb50e7c407962290a1a7fc289f98
Osten Power <osten_power@mail.ru>
2023-06-13 12:44:41 +0300

    add Dto for film and rating

---
+++ src/Services/Rating/Rating.BusinessLogic/DTOs/RatingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Rating.BusinessLogic.DTOs
+{
+    public class RatingDTO
+    {
+        public Guid Id { get; set; }
+        public int UserId { get; set; }
+        public int FilmId { get; set; }
+        public int Score { get; set; }
+    }
+}
