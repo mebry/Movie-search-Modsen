e41ddb37aefda74bee0698e0ef50e6e62f9be428
Osten Power <osten_power@mail.ru>
2023-06-13 12:45:14 +0300

    add filmService

---
+++ src/Services/Rating/Rating.BusinessLogic/Services/FilmServices/FilmService.cs
@@ -0,0 +1,93 @@
+﻿using Mapster;
+using Microsoft.Extensions.Logging;
+using Rating.BusinessLogic.DTOs;
+using Rating.BusinessLogic.Exceptions;
+using Rating.DataAccess.Entities;
+using Rating.DataAccess.Repositories.FilmRepositories;
+
+namespace Rating.BusinessLogic.Services.FilmServices
+{
+    internal class FilmService : IFilmService
+    {
+        private readonly IFilmRepository _filmRepository;
+        private ILogger<IFilmService> _logger;
+
+        public FilmService(IFilmRepository filmRepository, ILogger<IFilmService> logger)
+        {
+            _filmRepository = filmRepository;
+            _logger = logger;
+        }
+
+        public async Task<FilmDTO> Create(FilmDTO model)
+        {
+            var filmChecked = await _filmRepository.GetByIdAsync(model.Id);
+
+            if(filmChecked is not null)
+            {
+                _logger.LogError("The creation attempt failed. This id is already in use");
+
+                throw new NotFoundException("This id is already used");
+            }
+
+            var mapperModel = model.Adapt<Film>();
+
+            _filmRepository.Create(mapperModel);
+
+            await _filmRepository.SaveAsync();
+
+            return model;
+        }
+
+        public async Task<FilmDTO> Delete(FilmDTO model)
+        {
+            var filmChecked = await _filmRepository.GetByIdAsync(model.Id);
+
+            if(filmChecked is null)
+            {
+                _logger.LogError("The deletion attempt failed. This id is missing");
+
+                throw new NotFoundException("This id is missing");
+            }
+
+            _filmRepository.Delete(filmChecked);
+
+            await _filmRepository.SaveAsync();
+
+            return model;
+        }
+
+        public async Task<FilmDTO?> GetByIdAsync(Guid id)
+        {
+            var filmChecked = await _filmRepository.GetByIdAsync(id);
+
+            if(filmChecked is null)
+            {
+                _logger.LogError("The deletion attempt failed. This id is missing");
+
+                throw new NotFoundException("This id is missing");
+            }
+
+            var mappingModel = filmChecked.Adapt<FilmDTO>();
+
+            return mappingModel;
+        }
+
+        public async Task<FilmDTO> Update(FilmDTO model)
+        {
+            var filmChecked = await _filmRepository.GetByIdAsync(model.Id);
+
+            if(filmChecked is null)
+            {
+                _logger.LogError("The updating attempt failed. This id is missing");
+
+                throw new NotFoundException("This id is missing");
+            }
+
+            _filmRepository.Update(filmChecked);
+
+            await _filmRepository.SaveAsync();
+
+            return model;
+        }
+    }
+}
