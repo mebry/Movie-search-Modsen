2bc4a894d8b10d075e807f4cde4b2fb2551eb14d
Osten Power <osten_power@mail.ru>
2023-06-13 09:48:46 +0300

    add applicationcontext

---
+++ src/Services/Rating/Rating.DataAccess/Contexts/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rating.DataAccess.Entities;
+
+namespace Rating.DataAccess.Contexts
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Raiting> Raitings { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
