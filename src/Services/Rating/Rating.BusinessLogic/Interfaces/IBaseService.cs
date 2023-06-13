6486401de99306c7b8275be55f544f1f6fade0c4
Osten Power <osten_power@mail.ru>
2023-06-13 12:44:55 +0300

    add IBaseService

---
+++ src/Services/Rating/Rating.BusinessLogic/Interfaces/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿namespace Rating.BusinessLogic.Interfaces
+{
+    public interface IBaseService<T>
+    {
+        public Task<T> Create(T model);
+        public Task<T?> GetByIdAsync(Guid id);
+        public Task<T> Delete(T model);
+        public Task<T> Update(T model);
+    }
+}
