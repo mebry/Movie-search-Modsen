e7945d915142ab7c97f2f274138a84d5b573158a
Osten Power <osten_power@mail.ru>
2023-06-13 09:48:32 +0300

    adding film and raiting entities

---
+++ src/Services/Rating/Rating.DataAccess/Entities/Film.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rating.DataAccess.Entities
+{
+    public class Film
+    {
+        public Guid Id { get; set; }
+
+        [MinLength(1), MaxLength(10)]
+        public double AverageRaiting { get; set; }
+
+        [MinLength(0)]
+        public int CountOfScores { get; set; }
+
+        [MinLength(0)]
+        public int OldCountOfScores { get; set; }
+    }
+}
---
+++ src/Services/Rating/Rating.DataAccess/Entities/Raiting.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rating.DataAccess.Entities
+{
+    public class Raiting
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int? FilmId { get; set; }
+        public Film? Film { get; set; }
+
+        [MinLength(1), MaxLength(10)]
+        public int Score { get; set; }
+    }
+}
