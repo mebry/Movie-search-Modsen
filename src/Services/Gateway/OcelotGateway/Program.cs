a42b7c96139bb8b2558dad2787834cb5181837b7
labstudent <padalitsa2055@gmail.com>
2023-06-14 00:21:25 +0300

    Ocelot configured

--- src/Services/Gateway/OcelotGateway/Program.cs
+++ src/Services/Gateway/OcelotGateway/Program.cs
@@ -1,6 +1,14 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+await app.UseOcelot();
 
 app.Run();
