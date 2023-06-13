216c7eeb23a5585c350c8f147f59d24a5ef534da
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-13 15:17:35 +0300

    add staffPersonConfiguration

---
+++ src/Services/Staff/Staff.DataAccess/Configuration/StaffPersonConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Staff.DataAccess.Entities;
+
+namespace Staff.DataAccess.Configuration
+{
+    public class StaffPersonConfiguration : IEntityTypeConfiguration<StaffPerson>
+    {
+        public void Configure(EntityTypeBuilder<StaffPerson> builder)
+        {
+            builder.Property(x => x.StaffName)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.StaffSurname)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .IsRequired();
+        }
+    }
+}
