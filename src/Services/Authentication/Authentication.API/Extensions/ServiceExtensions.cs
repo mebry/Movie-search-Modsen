510a36bb71e019c05a558e678b88a7d3cac9dda8
labstudent <padalitsa2055@gmail.com>
2023-06-13 16:39:22 +0300

    Cors configured

--- src/Services/Authentication/Authentication.API/Extensions/ServiceExtensions.cs
+++ src/Services/Authentication/Authentication.API/Extensions/ServiceExtensions.cs
@@ -2,5 +2,16 @@
 {
     public static class ServiceExtensions
     {
+
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        }
     }
 }
--- src/Services/Authentication/Authentication.API/Program.cs
+++ src/Services/Authentication/Authentication.API/Program.cs
@@ -1,8 +1,11 @@
+using Authentication.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
 
