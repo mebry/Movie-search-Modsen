d2c0c853c5afc3270755bd8f7f26184e5ee58b04
Osten Power <osten_power@mail.ru>
2023-06-13 09:59:58 +0300

    add FilmRepository

---
+++ src/Services/Rating/Rating.DataAccess/Repositories/FilmRepositories/FilmRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rating.DataAccess.Contexts;
+using Rating.DataAccess.Entities;
+
+namespace Rating.DataAccess.Repositories.FilmRepositories
+{
+    internal class FilmRepository : IFilmRepository
+    {
+        private ApplicationContext _context;
+
+        public FilmRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Film entity)
+            => _context.Add(entity);
+
+        public void Delete(Film entity)
+            => _context.Remove(entity);
+
+        public IEnumerable<Film> GetAll()
+            => _context.Films
+                .AsNoTracking();
+
+        public async Task<Film?> GetByIdAsync(Guid entityId)
+             => await _context.Films
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == entityId);
+
+        public async Task SaveAsync()
+             => await _context.SaveChangesAsync();
+
+        public void Update(Film entity)
+             => _context.Update(entity);
+    }
+}
