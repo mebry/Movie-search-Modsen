2a237877981c239338af8bb262740437bb4748a4
Osten Power <osten_power@mail.ru>
2023-06-13 09:49:02 +0300

    add IRepository

---
+++ src/Services/Rating/Rating.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Rating.DataAccess.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        public void Create(TEntity item);
+        public Task<TEntity?> GetByIdAsync(Guid entityId);
+        public Task<IEnumerable<TEntity>> GetAllAsync();
+        public void Delete(Guid entityId);
+        public void Update(TEntity item);
+        public Task SaveAsync();
+    }
+}
