c27568ee179ac2c1359acf75b365609a9b3afc6f
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-13 14:02:08 +0300

    add entities

---
+++ src/Services/Staff/Staff.DataAccess/Entities/StaffPerson.cs
@@ -0,0 +1,14 @@
+﻿namespace Staff.DataAccess.Entities
+{
+    public class StaffPerson
+    {
+        public int Id { get; set; }
+        public string StaffName { get; set; }
+        public string StaffSurname { get; set; }
+        public string ImgUrl { get; set; }
+        public DateTimeOffset DateOfBirthday { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+
+    }
+}
---
+++ src/Services/Staff/Staff.DataAccess/Entities/StaffPersonPosition.cs
@@ -0,0 +1,10 @@
+﻿namespace Staff.DataAccess.Entities
+{
+    public class StaffPersonPosition
+    {
+        public int StaffPersonId { get; set; }
+        public StaffPerson StaffPerson { get; set; }
+        public int PositionId { get; set; }
+        public Position Position { get; set; }
+    }
+}
