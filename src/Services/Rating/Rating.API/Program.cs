7eb3243bbe33c903207274388fb401e7468d5a71
Osten Power <osten_power@mail.ru>
2023-06-14 09:59:19 +0300

    remove ConfigurationBuilder

--- src/Services/Rating/Rating.API/Program.cs
+++ src/Services/Rating/Rating.API/Program.cs
@@ -3,11 +3,6 @@ using Rating.BusinessLogic.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var configure = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddBusinessLogicService(builder.Configuration);
