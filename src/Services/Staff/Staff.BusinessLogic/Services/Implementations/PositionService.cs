57bcf7c8290f07c1abd470b43e308647066fba30
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-14 16:44:33 +0300

    services finished

---
+++ src/Services/Staff/Staff.BusinessLogic/Services/Implementations/PositionService.cs
@@ -0,0 +1,67 @@
+﻿using Mapster;
+using Microsoft.Extensions.Logging;
+using Staff.BusinessLogic.DTOs;
+using Staff.BusinessLogic.Exceptions;
+using Staff.BusinessLogic.Services.Interfaces;
+using Staff.DataAccess.Entities;
+using Staff.DataAccess.Repositories.Interfaces;
+
+namespace Staff.BusinessLogic.Services.Implementations
+{
+    public class PositionService : IPositionService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<PositionService> _logger;
+
+        public PositionService(IUnitOfWork unitOfWork, ILogger<PositionService> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public async Task<ResponsePositionDTO> CreateAsync(RequestPositionDTO requestPositionDTO)
+        {
+            var id = new Guid();
+            var mapperPosition = requestPositionDTO.Adapt<Position>();
+            mapperPosition.Id = id;
+            await _unitOfWork.PositionRepository.CreateAsync(mapperPosition);
+            await _unitOfWork.SaveChangesAsync();
+
+            var responseModel = mapperPosition.Adapt<ResponsePositionDTO>();
+
+            return responseModel;
+        }
+
+        public async Task<ResponsePositionDTO> GetPositionByIdAsync(Guid id)
+        {
+            var position = await _unitOfWork.PositionRepository.GetPositionByIdAsync(id);
+
+            if (position == null)
+            {
+                _logger.LogError($"Position with ID '{id}' not found.");
+
+                throw new NotFoundException("This id was not found");
+            }
+
+            var mapperPosition = position.Adapt<ResponsePositionDTO>();
+
+            return mapperPosition;
+        }
+
+        public async Task<IEnumerable<ResponsePositionDTO>> GetPositionsAsync()
+        {
+            var position = await _unitOfWork.PositionRepository.GetPositionsAsync();
+
+            if (position.Count() == 0)
+            {
+                _logger.LogError("Can't get all positions, there is no data");
+
+                throw new NotFoundException("There is no data");
+            }
+
+            var positions = position.Adapt<IEnumerable<ResponsePositionDTO>>();
+
+            return positions;
+        }
+    }
+}
---
+++ src/Services/Staff/Staff.BusinessLogic/Services/Interfaces/IFilmService.cs
@@ -0,0 +1,12 @@
+﻿using Staff.BusinessLogic.DTOs;
+
+namespace Staff.BusinessLogic.Services.Interfaces
+{
+    public interface IFilmService
+    {
+        public Task<ResponseFilmDTO> RemoveFilmAsync(Guid id);
+        public Task<ResponseFilmDTO> CreateAsync(RequestFilmDTO film);
+        public Task<ResponseFilmDTO> UpdateAsync(Guid id, RequestFilmDTO film);
+        public Task<ResponseFilmDTO> GetFilmByIdAsync(Guid id);
+    }
+}
