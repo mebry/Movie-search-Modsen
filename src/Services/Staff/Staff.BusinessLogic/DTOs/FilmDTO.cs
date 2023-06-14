e3287991960d26068e242129b37aa818c169ba59
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-14 12:08:04 +0300

    add DTOs

---
+++ src/Services/Staff/Staff.BusinessLogic/DTOs/FilmDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Staff.BusinessLogic.DTOs
+{
+    public class FilmDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateOnly ReleaseDate { get; set; }
+        public double AverageRating { get; set; }
+        public int CountOfScores { get; set; }
+    }
+}
---
+++ src/Services/Staff/Staff.BusinessLogic/DTOs/StaffPersonPositionDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Staff.BusinessLogic.DTOs
+{
+    public class StaffPersonPositionDTO
+    {
+        public Guid StaffPersonId { get; set; }
+        public PositionDTO Position { get; set; }
+        public FilmDTO Film { get; set; }
+    }
+}
