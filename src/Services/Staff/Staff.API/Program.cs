093cb03aca0bbb0441f97f25e969e24580ea7ced
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 14:20:24 +0300

    setting api

---
+++ src/Services/Staff/Staff.API/Program.cs
@@ -0,0 +1,28 @@
+using Staff.BusinessLogic.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBusinessLogicService(builder.Configuration);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
