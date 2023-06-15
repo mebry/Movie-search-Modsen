3b6594d7b879d43567fae3d638b0fcb87dbd88c5
mebry <vadushko@gmail.com>
2023-06-15 10:26:17 +0300

    Add the enum extension

---
+++ src/Services/Film/Film.BusinessLogic/Exceptions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Film.BusinessLogic.Exceptions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var field = value.GetType().GetField(value.ToString());
+            var attribute = field?.GetCustomAttribute<DescriptionAttribute>();
+            return attribute?.Description ?? value.ToString();
+        }
+    }
+}
