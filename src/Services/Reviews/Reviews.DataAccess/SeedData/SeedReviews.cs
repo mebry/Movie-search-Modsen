c81fe9f6167206da5b14dbf84500633cf5a7cb3a
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 16:52:58 +0300

    add seedData

---
+++ src/Services/Reviews/Reviews.DataAccess/SeedData/SeedReviews.cs
@@ -0,0 +1,11 @@
+﻿using Reviews.DataAccess.Entities;
+
+namespace Reviews.DataAccess.SeedData
+{
+    public static class SeedReviews
+    {
+        public static TypeOfReview PositiveTypeOfReview { get; } = new() { Id = Guid.NewGuid(), Name = "Positive" };
+        public static TypeOfReview NegativeTypeOfReview { get; } = new() { Id = Guid.NewGuid(), Name = "Negative" };
+        public static TypeOfReview NeutralTypeOfReview { get; } = new() { Id = Guid.NewGuid(), Name = "Neutral" };
+    }
+}
