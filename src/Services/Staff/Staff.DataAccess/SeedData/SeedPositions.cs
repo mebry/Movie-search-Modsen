2b52a08b27fec019c0818e4e6f51466e6fc66d54
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 14:19:18 +0300

    refactoring DAL

--- src/Services/Staff/Staff.DataAccess/SeedData/SeedPositions.cs
+++ src/Services/Staff/Staff.DataAccess/SeedData/SeedPositions.cs
@@ -4,12 +4,12 @@ namespace Staff.DataAccess.SeedData
 {
     public static class SeedPositions
     {
-        public static Position ActorPosition { get; } = new() { Id = new Guid(), Name = "Actor" };
-        public static Position RegisseurPosition { get; } = new() { Id = new Guid(), Name = "Regisseur" };
-        public static Position ProducerPosition { get; } = new() { Id = new Guid(), Name = "Producer" };
-        public static Position OperatorPosition { get; } = new() { Id = new Guid(), Name = "Operator" };
-        public static Position ComposerPosition { get; } = new() { Id = new Guid(), Name = "Composer" };
-        public static Position ArtistPosition { get; } = new() { Id = new Guid(), Name = "Artist" };
-        public static Position MontagePosition { get; } = new() { Id = new Guid(), Name = "Montage" };
+        public static Position ActorPosition { get; } = new() { Id = Guid.NewGuid(), Name = "Actor" };
+        public static Position RegisseurPosition { get; } = new() { Id = Guid.NewGuid(), Name = "Regisseur" };
+        public static Position ProducerPosition { get; } = new() { Id = Guid.NewGuid(), Name = "Producer" };
+        public static Position OperatorPosition { get; } = new() { Id = Guid.NewGuid(), Name = "Operator" };
+        public static Position ComposerPosition { get; } = new() { Id = Guid.NewGuid(), Name = "Composer" };
+        public static Position ArtistPosition { get; } = new() { Id = Guid.NewGuid(), Name = "Artist" };
+        public static Position MontagePosition { get; } = new() { Id = Guid.NewGuid(), Name = "Montage" };
     }
 }
