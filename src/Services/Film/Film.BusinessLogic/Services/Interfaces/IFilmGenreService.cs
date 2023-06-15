dead8acf9a5c997485e34102deeeb3b32f27ef88
mebry <vadushko@gmail.com>
2023-06-15 21:49:22 +0300

    Add interfaces for services

---
+++ src/Services/Film/Film.BusinessLogic/Services/Interfaces/IFilmGenreService.cs
@@ -0,0 +1,26 @@
+﻿using Film.BusinessLogic.DTOs.RequestDTOs;
+using Film.BusinessLogic.DTOs.ResponseDTOs;
+
+namespace Film.BusinessLogic.Services.Interfaces
+{
+    /// <summary>
+    /// Service interface for film genres.
+    /// </summary>
+    public interface IFilmGenreService
+    {
+        /// <summary>
+        /// Creates a new film genre.
+        /// </summary>
+        /// <param name="filmGenre">The film genre data.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task CreateAsync(FilmGenreRequestDTO filmGenre);
+
+        /// <summary>
+        /// Deletes a film genre.
+        /// </summary>
+        /// <param name="filmId">The ID of the film.</param>
+        /// <param name="genreId">The ID of the genre.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task DeleteAsync(Guid filmId, Guid genreId);
+    }
+}
---
+++ src/Services/Film/Film.BusinessLogic/Services/Interfaces/IFilmTagService.cs
@@ -0,0 +1,26 @@
+﻿using Film.BusinessLogic.DTOs.RequestDTOs;
+using System.Reflection.Metadata;
+
+namespace Film.BusinessLogic.Services.Interfaces
+{
+    /// <summary>
+    /// Service interface for film tags.
+    /// </summary>
+    public interface IFilmTagService
+    {
+        /// <summary>
+        /// Creates a new film tag.
+        /// </summary>
+        /// <param name="filmTag">The film tag data.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task CreateAsync(FilmTagRequestDTO filmTag);
+
+        /// <summary>
+        /// Deletes a film tag.
+        /// </summary>
+        /// <param name="filmId">The ID of the film.</param>
+        /// <param name="tagId">The ID of the tag.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task DeleteAsync(Guid filmId, Guid tagId);
+    }
+}
