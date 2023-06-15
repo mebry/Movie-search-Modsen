998f4c096a3bf497f447000ce0307b73f172e93a
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 16:54:14 +0300

    add configurationExtensions

---
+++ src/Services/Reviews/Reviews.DataAccess/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Reviews.DataAccess.Contexts;
+using Reviews.DataAccess.Repositories.Implementations;
+using Reviews.DataAccess.Repositories.Interfaces;
+
+namespace Reviews.DataAccess.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static void AddDataAccessService(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddConfigurationMSSQLServer(configuration);
+            service.AddRepositoriesService();
+        }
+        private static void AddConfigurationMSSQLServer(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<ReviewsDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("Reviews.DataAccess")));
+        }
+        public static void AddRepositoriesService(this IServiceCollection service)
+        {
+            service.AddScoped<ICriticRepository, CriticRepository>();
+            service.AddScoped<IReviewRepository, ReviewRepository>();
+            service.AddScoped<ITypeOfReviewRepository, TypeOfReviewRepository>();
+            service.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
