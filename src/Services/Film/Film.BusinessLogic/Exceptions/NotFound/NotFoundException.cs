0e7e5ae864bde58c213d50edd8ca9f2b00afa81c
mebry <vadushko@gmail.com>
2023-06-15 21:48:37 +0300

    Add auto-comments to the exceptions

--- src/Services/Film/Film.BusinessLogic/Exceptions/NotFound/NotFoundException.cs
+++ src/Services/Film/Film.BusinessLogic/Exceptions/NotFound/NotFoundException.cs
@@ -1,5 +1,8 @@
 ﻿namespace Film.BusinessLogic.Exceptions.NotFound
 {
+    /// <summary>
+    /// Represents an exception that is thrown when an age restriction is not found.
+    /// </summary>
     public class NotFoundException : Exception
     {
         public NotFoundException(string message) : base(message) { }
--- src/Services/Film/Film.BusinessLogic/Exceptions/NotFound/TagNotFoundException.cs
+++ src/Services/Film/Film.BusinessLogic/Exceptions/NotFound/TagNotFoundException.cs
@@ -1,5 +1,8 @@
 ﻿namespace Film.BusinessLogic.Exceptions.NotFound
 {
+    /// <summary>
+    /// Represents an exception that is thrown when an age restriction is not found.
+    /// </summary>
     public class TagNotFoundException : NotFoundException
     {
         public TagNotFoundException(Guid id)
