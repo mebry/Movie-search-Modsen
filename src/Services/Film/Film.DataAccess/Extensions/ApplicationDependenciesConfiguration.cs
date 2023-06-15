1c59f4caf42e63698dfddf9980006f1ed74e9ed4
mebry <vadushko@gmail.com>
2023-06-15 09:16:40 +0300

    Add extensions. The first one is AddFilmDatabase for adding a database. The second one is AddRepositories for adding interfaces and their implementations.

---
+++ src/Services/Film/Film.DataAccess/Extensions/ApplicationDependenciesConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Film.DataAccess.Contexts;
+using Film.DataAccess.Repositories.Implementations;
+using Film.DataAccess.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Film.DataAccess.Extensions
+{
+    public static class ApplicationDependenciesConfiguration
+    {
+        public static IServiceCollection AddFilmDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FilmContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                builder => builder.MigrationsAssembly(typeof(FilmContext).Assembly.FullName)));
+            
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            return services.AddScoped<IAgeRestrictionRepository, AgeRestrictionRepository>()
+                .AddScoped<IFilmCountryRepository, FilmCountryRepository>()
+                .AddScoped<IFilmGenreRepository, FilmGenreRepository>()
+                .AddScoped<IFilmRepository, FilmRepository>()
+                .AddScoped<IFilmTagRepository, FilmTagRepository>()
+                .AddScoped<IGenreRepository, GenreRepository>()
+                .AddScoped<IPositionRepository, IPositionRepository>()
+                .AddScoped<IStaffPersonPositionRepository, StaffPersonPositionRepository>()
+                .AddScoped<IStaffPersonRepository, StaffPersonRepository>()
+                .AddScoped<ITagRepository, TagRepository>();
+        }
+    }
+}
