c92a9f41f595eea082c2c66ffc169dd3f6d51b75
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 16:53:23 +0300

    add dbContext

---
+++ src/Services/Reviews/Reviews.DataAccess/Contexts/ReviewsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reviews.DataAccess.Entities;
+using System.Reflection;
+
+namespace Reviews.DataAccess.Contexts
+{
+    public class ReviewsDbContext : DbContext
+    {
+        public virtual DbSet<Critic> Critics { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+        public virtual DbSet<TypeOfReview> TypesOfReview { get; set; }
+        public ReviewsDbContext(DbContextOptions<ReviewsDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
