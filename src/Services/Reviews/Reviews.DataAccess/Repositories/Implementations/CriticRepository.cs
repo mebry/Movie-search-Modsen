ccfb0c13dc1fce633a42954ac095e43aa8fda50f
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 16:54:32 +0300

    add repositories

---
+++ src/Services/Reviews/Reviews.DataAccess/Repositories/Implementations/CriticRepository.cs
@@ -0,0 +1,31 @@
+﻿using Reviews.DataAccess.Contexts;
+using Reviews.DataAccess.Entities;
+using Reviews.DataAccess.Repositories.Interfaces;
+
+namespace Reviews.DataAccess.Repositories.Implementations
+{
+    public class CriticRepository : ICriticRepository
+    {
+        private readonly ReviewsDbContext _dbContext;
+
+        public CriticRepository(ReviewsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task CreateAsync(Critic critic)
+        {
+            await _dbContext.Critics.AddAsync(critic);
+        }
+
+        public void RemoveCritic(Critic critic)
+        {
+            _dbContext.Critics.Remove(critic);
+        }
+
+        public void Update(Critic critic)
+        {
+            _dbContext.Critics.Update(critic);
+        }
+    }
+}
---
+++ src/Services/Reviews/Reviews.DataAccess/Repositories/Interfaces/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using Reviews.DataAccess.Entities;
+
+namespace Reviews.DataAccess.Repositories.Interfaces
+{
+    public interface IReviewRepository
+    {
+        public Task CreateAsync(Review review);
+        public void Update(Review review);
+        public void RemoveReview(Review review);
+        public Task<IEnumerable<Review>> GetAllByFilmIdAsync(Guid filmId);
+        public Task<IEnumerable<Review>> GetAllByCriticIdAsync(Guid criticId);
+        public Task<IEnumerable<Review>> GetAllByFilmIdAndType(Guid filmId, Guid typeOfReviewId);
+    }
+}
