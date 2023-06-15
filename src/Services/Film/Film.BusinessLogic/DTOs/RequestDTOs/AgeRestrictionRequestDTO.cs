ca452cdd5aa8e3ca870e35524bb2db76ecdf2811
mebry <vadushko@gmail.com>
2023-06-15 10:17:31 +0300

    Add requestDTOs

---
+++ src/Services/Film/Film.BusinessLogic/DTOs/RequestDTOs/AgeRestrictionRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Film.BusinessLogic.DTOs.RequestDTOs
+{
+    /// <summary>
+    /// Data transfer object for AgeRestriction requests.
+    /// </summary>
+    public class AgeRestrictionRequestDTO
+    {
+        /// <summary>
+        /// The age value for the AgeRestriction.
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
