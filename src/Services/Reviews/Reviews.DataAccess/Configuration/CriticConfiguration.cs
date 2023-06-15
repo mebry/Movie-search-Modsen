bd5d1c62ac9c04831aa016f4e05f73be03410e8c
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-15 16:53:43 +0300

    add configurations

---
+++ src/Services/Reviews/Reviews.DataAccess/Configuration/CriticConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Reviews.DataAccess.Entities;
+
+namespace Reviews.DataAccess.Configuration
+{
+    public class CriticConfiguration : IEntityTypeConfiguration<Critic>
+    {
+        public void Configure(EntityTypeBuilder<Critic> builder)
+        {
+            builder.Property(x => x.Username).IsRequired();
+        }
+    }
+}
