eeb2041fdf3e7becbe4c9649141a79bb91007a8a
labstudent <padalitsa2055@gmail.com>
2023-06-16 08:25:34 +0300

    Context configured

---
+++ src/Services/FilmCollection/FilmCollection.DataAccess/Contexts/FilmCollectionContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.DataAccess.Contexts
+{
+    internal class FilmCollectionContext : DbContext
+    {
+        public FilmCollectionContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
