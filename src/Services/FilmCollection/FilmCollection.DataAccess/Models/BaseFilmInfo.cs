22701b90281a3db5601666d4708091106e038288
labstudent <padalitsa2055@gmail.com>
2023-06-16 08:14:57 +0300

    Models configured

---
+++ src/Services/FilmCollection/FilmCollection.DataAccess/Models/BaseFilmInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.DataAccess.Models
+{
+    public class BaseFilmInfo
+    {
+        public Guid Id { get; set; }
+        public string Tiltle { get; set; }
+        public string PosterUrl { get; set; }   
+        public DateOnly ReleaseDate { get; set; }   
+        public TimeSpan Duration { get; set; }
+        public double AverageRating { get; set; }
+        public int CountOfScores { get; set; }  
+        public ICollection<FilmCollection> Collections { get; set;}
+    }
+}
