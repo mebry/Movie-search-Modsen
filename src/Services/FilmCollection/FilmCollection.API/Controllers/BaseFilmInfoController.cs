9f8e730f7d7ed3fca85952277349d0ad4cdeb510
labstudent <padalitsa2055@gmail.com>
2023-06-18 01:27:06 +0300

    BaseFilmInfoController created.

---
+++ src/Services/FilmCollection/FilmCollection.API/Controllers/BaseFilmInfoController.cs
@@ -0,0 +1,26 @@
+﻿using FilmCollection.BusinessLogic.DTOs.RequestDTOs;
+using FilmCollection.BusinessLogic.Services.Interfaces;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmCollection.API.Controllers
+{
+    [Route("api/v1/films")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "v1")]
+    public class BaseFilmInfoController
+    {
+        private readonly IBaseFilmInfoService _baseFilmInfoService;
+        private readonly IValidator<BaseFilmInfoRequestDto> _validator;
+        private readonly ILogger _logger;
+
+        public BaseFilmInfoController(IBaseFilmInfoService baseFilmInfoService, IValidator<BaseFilmInfoRequestDto> validator, ILogger logger)
+        {
+            _baseFilmInfoService = baseFilmInfoService;
+            _validator = validator;
+            _logger = logger;
+        }
+
+
+    }
+}
