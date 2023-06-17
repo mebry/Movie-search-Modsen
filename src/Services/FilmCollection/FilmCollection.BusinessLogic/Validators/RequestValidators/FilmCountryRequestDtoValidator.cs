0c47fbacbebf98486917b96d36d25b219ccf309d
labstudent <padalitsa2055@gmail.com>
2023-06-17 21:43:05 +0300

    FilmCountry validator implemented

--- src/Services/FilmCollection/FilmCollection.BusinessLogic/Validators/RequestValidators/FilmCountryRequestDtoValidator.cs
+++ src/Services/FilmCollection/FilmCollection.BusinessLogic/Validators/RequestValidators/FilmCountryRequestDtoValidator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FilmCollection.DataAccess.Models;
+using FluentValidation;
+using Shared.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace FilmCollection.BusinessLogic.Validators.RequestValidators
 {
-    internal class FilmCountryRequestDtoValidator
+    internal class FilmCountryRequestDtoValidator : AbstractValidator<FilmCountry>
     {
+        public FilmCountryRequestDtoValidator()
+        {
+            RuleFor(fc => fc.CountryId)
+                .NotNull()
+                .WithMessage("Country id is required")
+                .IsInEnum()
+                .WithMessage("Invalid country");
+
+            RuleFor(fc => fc.BaseFilmInfoId)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Film id is required");
+        }
     }
 }
