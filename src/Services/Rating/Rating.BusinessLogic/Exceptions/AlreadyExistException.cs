4eac48badc01dc611892ec3948edc26348ef8096
Osten Power <osten_power@mail.ru>
2023-06-19 10:42:03 +0300

    add shared project

--- src/Services/Rating/Rating.BusinessLogic/Exceptions/AlreadyExistException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rating.BusinessLogic.Exceptions
-{
-    public class AlreadyExistException : Exception
-    {
-        public AlreadyExistException(string message) : base(message)
-        {
-        }
-    }
-}
--- src/Services/Rating/Rating.BusinessLogic/Exceptions/ValidationProblemException .cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Rating.BusinessLogic.Exceptions
-{
-    public class ValidationProblemException : Exception
-    {
-        public ValidationProblemException(string message) : base(message)
-        {
-        }
-    }
-}
