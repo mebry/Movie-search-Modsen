aa8fc96b637d6f2bd2f199db79d0d919b5898715
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-19 16:21:46 +0300

    add controllers

---
+++ src/Services/Reviews/Reviews.API/Controllers/CriticController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reviews.BusinessLogic.DTOs;
+using Reviews.BusinessLogic.Services.Interfaces;
+
+namespace Reviews.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CriticController : ControllerBase
+    {
+        private readonly ICriticService _criticService;
+
+        public CriticController(ICriticService criticService)
+        {
+            _criticService = criticService;
+        }
+
+        /// <summary>
+        /// Creates a new critic.
+        /// </summary>
+        /// <param name="requestCriticDTO">The critic data to create.</param>
+        /// <returns>Returns the created critic.</returns>
+        [HttpPost]
+        public async Task<ActionResult<ResponseCriticDTO>> CreateAsync(RequestCriticDTO requestCriticDTO)
+        {
+            var result = await _criticService.CreateAsync(requestCriticDTO);
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Get a critic by ID.
+        /// </summary>
+        /// <param name="id">The ID of the critic.</param>
+        /// <returns>Returns the critic with the specified ID.</returns>
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ResponseCriticDTO>> GetByIdAsync(Guid id)
+        {
+            var result = await _criticService.GetByIdAsync(id);
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Removes a critic by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the critic to remove.</param>
+        /// <returns>Returns the removed critic.</returns>
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ResponseCriticDTO>> RemoveAsync(Guid id)
+        {
+            var result = await _criticService.RemoveCriticAsync(id);
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Updates an existing critic.
+        /// </summary>
+        /// <param name="id">The ID of the critic to update.</param>
+        /// <param name="requestCriticDTO">The updated critic data.</param>
+        /// <returns>Returns the updated critic.</returns>
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<ResponseCriticDTO>> UpdateAsync(Guid id, RequestCriticDTO requestCriticDTO)
+        {
+            var result = await _criticService.UpdateAsync(id, requestCriticDTO);
+
+            return Ok(result);
+        }
+    }
+}
