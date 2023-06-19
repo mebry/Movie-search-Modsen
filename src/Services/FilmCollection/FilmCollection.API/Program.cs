4834930f90a48dfb7ca3664ed3545b61355566c3
labstudent <padalitsa2055@gmail.com>
2023-06-19 23:56:04 +0300

    Add api configuration.

--- src/Services/FilmCollection/FilmCollection.API/Program.cs
+++ src/Services/FilmCollection/FilmCollection.API/Program.cs
@@ -1,16 +1,34 @@
+using FilmCollection.API.Middlewares;
+using Microsoft.AspNetCore.HttpOverrides;
+using FilmCollection.BusinessLogic.Extensions;
+using FilmCollection.DataAccess.Extensions;
+using FilmCollection.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.ConfigureDataAccess(builder.Configuration);
+builder.Services.ConfigureBusinessLogic();
+builder.Services.ConfigureAPI();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.ConfigureExceptionHandler();
 
+if (app.Environment.IsProduction())
+    app.UseHsts();
+
+app.UseSwagger();
+app.UseSwaggerUI(s =>
+{
+    s.SwaggerEndpoint("/swagger/v1/swagger.json", "FilmCollection API v1");
+});
 app.UseHttpsRedirection();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
 
-app.UseAuthorization();
+app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
