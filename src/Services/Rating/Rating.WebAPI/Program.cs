2848d197320ca3d7b47aaaffd7908f6db70cedfd
Osten Power <osten_power@mail.ru>
2023-06-20 14:24:41 +0300

    setting sqlservice

--- src/Services/Rating/Rating.WebAPI/Program.cs
+++ src/Services/Rating/Rating.WebAPI/Program.cs
@@ -1,15 +1,15 @@
 using Hangfire;
 using Rating.API.Extensions;
 using Rating.BusinessLogic.Extensions;
+using Rating.DataAccess.Extensions;
 using Shared.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddBusinessLogicService(builder.Configuration);
+builder.Services.AddBusinessLogicService();
 builder.Services.AddSwaggerGen();
-
 builder.Services.ConfigureHagfire(builder.Configuration);
 
 var app = builder.Build();
@@ -32,5 +32,7 @@ app.UseHangfireDashboard();
 app.MapHangfireDashboard();
 
 app.UseHangfireRecurringJobs(builder.Configuration);
+
+app.ApplyMigrations();
 
 app.Run();
