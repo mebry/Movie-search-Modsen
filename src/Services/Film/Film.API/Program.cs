dd3dde043fba752d40dfeffe1e0a56dc5f19bd9f
mebry <vadushko@gmail.com>
2023-06-20 21:10:45 +0300

    Add a new layer responsible for the web API

---
+++ src/Services/Film/Film.API/Program.cs
@@ -0,0 +1,34 @@
+using Film.API.Extensions;
+using Shared.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureCors();
+
+builder.ConfigureServices();
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
