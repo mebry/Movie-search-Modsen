4e43546c336ce3fe6a8e71369f6f67bbe6441b1b
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-20 14:22:11 +0300

    refactoring DAL

--- src/Services/Reviews/Reviews.DataAccess/Contexts/ReviewsDbContext.cs
+++ src/Services/Reviews/Reviews.DataAccess/Contexts/ReviewsDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Reviews.DataAccess.Entities;
 using System.Reflection;
 
@@ -6,13 +7,22 @@ namespace Reviews.DataAccess.Contexts
 {
     public class ReviewsDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
         public virtual DbSet<Critic> Critics { get; set; }
         public virtual DbSet<Review> Reviews { get; set; }
         public virtual DbSet<TypeOfReview> TypesOfReview { get; set; }
 
-        public ReviewsDbContext(DbContextOptions<ReviewsDbContext> options)
-            : base(options)
+        public ReviewsDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
         {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
