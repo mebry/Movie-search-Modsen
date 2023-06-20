db644b2ee47c55159227f63c5f8fca8b7112145d
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-20 14:48:14 +0300

    refactoring

--- src/Services/Reviews/Reviews.DataAccess/Extensions/ConfigurationExtensions.cs
+++ src/Services/Reviews/Reviews.DataAccess/Extensions/ConfigurationExtensions.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Reviews.DataAccess.Contexts;
 using Reviews.DataAccess.Repositories.Implementations;
 using Reviews.DataAccess.Repositories.Interfaces;
 
@@ -11,16 +9,8 @@ namespace Reviews.DataAccess.Extensions
     {
         public static void AddDataAccessService(this IServiceCollection service, IConfiguration configuration)
         {
-            //service.AddConfigurationMSSQLServer(configuration);
             service.AddRepositoriesService();
         }
-
-        //private static void AddConfigurationMSSQLServer(this IServiceCollection service, IConfiguration configuration)
-        //{
-        //    service.AddDbContext<ReviewsDbContext>(options =>
-        //        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
-        //            b => b.MigrationsAssembly("Reviews.DataAccess")));
-        //}
 
         private static void AddRepositoriesService(this IServiceCollection service)
         {
