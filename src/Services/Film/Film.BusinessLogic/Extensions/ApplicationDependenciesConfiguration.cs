9020191655a33cb64b1526ca14c1458e814604a1
mebry <vadushko@gmail.com>
2023-06-20 09:56:41 +0300

    Add an extension method called ConfigureValidators to bind validators for DTOs

--- src/Services/Film/Film.BusinessLogic/Extensions/ApplicationDependenciesConfiguration.cs
+++ src/Services/Film/Film.BusinessLogic/Extensions/ApplicationDependenciesConfiguration.cs
@@ -1,5 +1,8 @@
-﻿using Film.BusinessLogic.Services.Implementations;
+﻿using Film.BusinessLogic.DTOs.RequestDTOs;
+using Film.BusinessLogic.ModelValidators.RequestDTOs;
+using Film.BusinessLogic.Services.Implementations;
 using Film.BusinessLogic.Services.Interfaces;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Film.BusinessLogic.Extensions
@@ -15,6 +18,20 @@ namespace Film.BusinessLogic.Extensions
                 .AddScoped<IFilmTagService, FilmTagService>()
                 .AddScoped<IGenreService, GenreService>()
                 .AddScoped<ITagService, TagService>();
+        }
+
+        public static IServiceCollection ConfigureValidators(this IServiceCollection services)
+        {
+            return services.AddScoped<IValidator<AgeRestrictionRequestDTO>, AgeRestrictionRequestDTOValidator>()
+                .AddScoped<IValidator<FilmCountryRequestDTO>, FilmCountryRequestDTOValidator>()
+                .AddScoped<IValidator<FilmGenreRequestDTO>, FilmGenreRequestDTOValidator>()
+                .AddScoped<IValidator<FilmRequestDTO>, FilmRequestDTOValidator>()
+                .AddScoped<IValidator<FilmTagRequestDTO>, FilmTagRequestDTOValidator>()
+                .AddScoped<IValidator<GenreRequestDTO>, GenreRequestDTOValidator>()
+                .AddScoped<IValidator<PositionRequestDTO>, PositionRequestDTOValidator>()
+                .AddScoped<IValidator<StaffPersonPositionRequestDTO>, StaffPersonPositionRequestDTOValidator>()
+                .AddScoped<IValidator<StaffPersonRequestDTO>, StaffPersonRequestDTOValidator>()
+                .AddScoped<IValidator<TagRequestDTO>, TagRequestDTOValidator>();
         }
     }
 }
