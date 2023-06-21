6c9b9c6db5bd20f33158fb10b7ed69024543c359
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:25:58 +0300

    Created static class for containing some constants values using in request parameters describers.

---
+++ src/Services/FilmCollection/FilmCollection.Shared/Constants/RequestParametersConstants/RequestParametersConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Shared.Constants.RequestParametersConstants
+{
+    public class RequestParametersConstants
+    {
+        public const int DEFAULT_MAX_PAGE_SIZE = 20;
+
+        public const int DEFAULT_PAGE_NUMBER = 1;
+
+        public const int DEFAULT_PAGE_SIZE = 10;
+    }
+}
