eb92b25b11cec2d2cad17b073c1e14bfe5efe94b
Osten Power <osten_power@mail.ru>
2023-06-21 12:21:20 +0300

    validator for dto

--- src/Services/Rating/Rating.BusinessLogic/Extensions/ServiceProviderExtensions.cs
+++ src/Services/Rating/Rating.BusinessLogic/Extensions/ServiceProviderExtensions.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
+using Rating.BusinessLogic.DTOs;
 using Rating.BusinessLogic.Services.AlgorithmServices;
 using Rating.BusinessLogic.Services.EventDecisionServices;
 using Rating.BusinessLogic.Services.EventDispatchServices;
 using Rating.BusinessLogic.Services.FilmServices;
 using Rating.BusinessLogic.Services.RatingServices;
+using Rating.BusinessLogic.Validators;
 using Rating.DataAccess.Extensions;
 
 namespace Rating.BusinessLogic.Extensions
@@ -15,6 +17,7 @@ namespace Rating.BusinessLogic.Extensions
         {
             services.AddDataAccessService();
             services.AddServices();
+            services.AddValidators();
         }
 
         private static void AddServices(this IServiceCollection services)
@@ -24,6 +27,12 @@ namespace Rating.BusinessLogic.Extensions
             services.AddScoped<IEventDispatchService, EventDispatchService>();
             services.AddScoped<IEventDecisionService, EventDecisionService>();
             services.AddScoped<IAlgorithmsForEventDecisionService, AlgorithmsForEventDecisionService>();
+        }
+
+        private static void AddValidators(this IServiceCollection services)
+        {
+            services.AddScoped<IValidator<FilmDTO>, FilmDTOValidator>();
+            services.AddScoped<IValidator<RequestRatingDTO>, ResponseRatingDTOValidator>();
         }
     }
 }
--- src/Services/Rating/Rating.DataAccess/Extensions/ServiceProviderExtensions.cs
+++ src/Services/Rating/Rating.DataAccess/Extensions/ServiceProviderExtensions.cs
@@ -1,12 +1,7 @@
-﻿using FluentValidation;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Rating.DataAccess.Contexts;
-using Rating.DataAccess.Entities;
 using Rating.DataAccess.Repositories.FilmRepositories;
 using Rating.DataAccess.Repositories.RaitingRepositories;
-using Rating.DataAccess.Validators;
 
 namespace Rating.DataAccess.Extensions
 {
@@ -16,9 +11,6 @@ namespace Rating.DataAccess.Extensions
         {
             service.AddDbContext<ApplicationContext>();
             service.AddRepositoriesService();
-
-            service.AddScoped<IValidator<Film>, FilmValidator>();
-            service.AddScoped<IValidator<RatingFilm>, RatingFilmValidator>();
         }
 
         private static void AddRepositoriesService(this IServiceCollection service)
