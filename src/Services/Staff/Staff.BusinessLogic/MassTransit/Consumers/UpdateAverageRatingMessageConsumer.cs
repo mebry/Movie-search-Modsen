00394e9609c660b4cd463be887a9252e1b22bea8
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-21 16:02:18 +0300

    add consumers

---
+++ src/Services/Staff/Staff.BusinessLogic/MassTransit/Consumers/UpdateAverageRatingMessageConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared.Messages;
+using Staff.DataAccess.Entities;
+using Staff.DataAccess.Repositories.Interfaces;
+
+namespace Staff.BusinessLogic.MassTransit.Consumers
+{
+    public class UpdateAverageRatingMessageConsumer : IConsumer<UpdateAverageRatingMessage>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<UpdateAverageRatingMessageConsumer> _logger;
+
+        public UpdateAverageRatingMessageConsumer(IUnitOfWork unitOfWork, ILogger<UpdateAverageRatingMessageConsumer> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<UpdateAverageRatingMessage> context)
+        {
+            var film = new Film
+            {
+                Id = context.Message.FilmId,
+                AverageRating = context.Message.AverageRating
+            };
+
+            _unitOfWork.FilmRepository.Update(film);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/Services/Staff/Staff.BusinessLogic/MassTransit/Consumers/UpdateCountOfScoresMessageConsumer.cs
@@ -0,0 +1,32 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared.Messages;
+using Staff.DataAccess.Entities;
+using Staff.DataAccess.Repositories.Interfaces;
+
+namespace Staff.BusinessLogic.MassTransit.Consumers
+{
+    public class UpdateCountOfScoresMessageConsumer : IConsumer<UpdateCountOfScoresMessage>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<UpdateCountOfScoresMessageConsumer> _logger;
+
+        public UpdateCountOfScoresMessageConsumer(IUnitOfWork unitOfWork, ILogger<UpdateCountOfScoresMessageConsumer> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<UpdateCountOfScoresMessage> context)
+        {
+            var film = new Film
+            {
+                Id = context.Message.FilmId,
+                CountOfScores = context.Message.CountOfScores
+            };
+
+            _unitOfWork.FilmRepository.Update(film);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
