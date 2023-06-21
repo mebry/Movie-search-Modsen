cca3f37ba1bbdbc7698343af6af110732631683e
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:17:14 +0300

    Add mapping configurations scan across the current assembly.

--- src/Services/FilmCollection/FilmCollection.BusinessLogic/Extensions/BusinessLogicDependencyInjection.cs
+++ src/Services/FilmCollection/FilmCollection.BusinessLogic/Extensions/BusinessLogicDependencyInjection.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -53,6 +54,7 @@ namespace FilmCollection.BusinessLogic.Extensions
         private static void ConfigureMappings(this IServiceCollection services)
         {
             var config = TypeAdapterConfig.GlobalSettings;
+            config.Scan(Assembly.GetExecutingAssembly());
             services.AddSingleton(config);
             services.AddScoped<IMapper, ServiceMapper>(); 
         }
