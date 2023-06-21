1e06672145ab1e978bef1cf7bdfae02742342fa7
Osten Power <osten_power@mail.ru>
2023-06-21 14:06:13 +0300

    add message for rating

---
+++ src/Shared/Messages/UpdateAverageRatingMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Messages
+{
+    public class UpdateAverageRatingMessage
+    {
+        public Guid FilmId { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
