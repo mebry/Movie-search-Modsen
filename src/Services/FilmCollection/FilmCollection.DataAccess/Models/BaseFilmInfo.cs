d4a5b0ca81e556306864b837e2c1ec15e8ab4177
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:55:38 +0300

    Fixed formatting.

--- src/Services/FilmCollection/FilmCollection.DataAccess/Models/BaseFilmInfo.cs
+++ src/Services/FilmCollection/FilmCollection.DataAccess/Models/BaseFilmInfo.cs
@@ -4,13 +4,13 @@
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string PosterURL { get; set; }   
-        public DateOnly ReleaseDate { get; set; }   
+        public string PosterURL { get; set; }
+        public DateOnly ReleaseDate { get; set; }
         public TimeSpan Duration { get; set; }
         public double AverageRating { get; set; }
-        public int NumberOfRatings { get; set; }  
-        public ICollection<FilmCollection> FilmCollections { get; set;}
-        public ICollection<FilmGenre> FilmGenres { get; set;}
+        public int NumberOfRatings { get; set; }
+        public ICollection<FilmCollection> FilmCollections { get; set; }
+        public ICollection<FilmGenre> FilmGenres { get; set; }
         public ICollection<FilmCountry> FilmCountries { get; set; }
     }
 }
