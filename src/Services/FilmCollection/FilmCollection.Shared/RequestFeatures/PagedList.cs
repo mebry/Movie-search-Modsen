f933c4d662e0be16bd2b81200be229e1abdb8cef
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:26:50 +0300

    Added pagination wrappers that contain pagination specific information.

---
+++ src/Services/FilmCollection/FilmCollection.Shared/RequestFeatures/PagedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Shared.RequestFeatures
+{
+    public class PagedList<T> : List<T>
+    {
+        public MetaData MetaData { get; set; }
+
+        public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+        {
+            MetaData = new MetaData
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+            };
+
+            AddRange(items);
+        }
+
+        public static PagedList<T> ToPagedList(IEnumerable<T> items, int pageNumber, int pageSize) 
+        {
+            var count = items.Count();
+            return new PagedList<T>(items.ToList(), count, pageNumber, pageSize);
+        }
+    }
+}
