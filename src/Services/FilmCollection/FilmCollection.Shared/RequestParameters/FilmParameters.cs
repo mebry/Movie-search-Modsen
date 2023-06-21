f2e857b3772a91a50f7b095c99866cd8282f2d07
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:27:30 +0300

    Describing request query parameters that used in filtering, sorting and pagination.

---
+++ src/Services/FilmCollection/FilmCollection.Shared/RequestParameters/FilmParameters.cs
@@ -0,0 +1,25 @@
+﻿using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Shared.RequestParameters
+{
+    public class FilmParameters : RequestParameters
+    {
+        public FilmParameters() 
+        {
+            OrderBy = "title";
+        }
+
+        public string SearchTerm { get; set; }
+        public Guid GenreId { get; set; }
+        public Countries CountryId { get; set; } 
+        public Guid CollectionId { get; set; }
+        public uint MinReleaseYear { get; set; } = 1;
+        public uint MaxReleaseYear { get; set; } = (uint)(DateTime.Now.Year + 10);
+
+    }
+}
---
+++ src/Services/FilmCollection/FilmCollection.Shared/RequestParameters/RequestParameters.cs
@@ -0,0 +1,26 @@
+﻿using FilmCollection.Shared.Constants.RequestParametersConstants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.Shared.RequestParameters
+{
+    public abstract class RequestParameters
+    {
+        public int MaxPageSize { get; init; } = RequestParametersConstants.DEFAULT_MAX_PAGE_SIZE;
+
+        public int PageNumber { get; set; } = RequestParametersConstants.DEFAULT_PAGE_NUMBER;
+
+        private int _pageSize = RequestParametersConstants.DEFAULT_PAGE_SIZE;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+        public string OrderBy { get; set; }
+    }
+}
