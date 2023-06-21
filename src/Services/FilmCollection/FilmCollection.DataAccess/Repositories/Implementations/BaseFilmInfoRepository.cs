96d8da96e28c824f4e4f6dd479fa1dc1e151315c
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:23:59 +0300

    Added filtration, pagination and sorting using iqueryable expressions.

--- src/Services/FilmCollection/FilmCollection.DataAccess/Repositories/Implementations/BaseFilmInfoRepository.cs
+++ src/Services/FilmCollection/FilmCollection.DataAccess/Repositories/Implementations/BaseFilmInfoRepository.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FilmCollection.Shared.RequestParameters;
+using FilmCollection.DataAccess.Extensions;
+using FilmCollection.Shared.RequestFeatures;
 
 namespace FilmCollection.DataAccess.Repositories.Implementations
 {
@@ -34,10 +37,30 @@ namespace FilmCollection.DataAccess.Repositories.Implementations
 
         public async Task<BaseFilmInfo> GetBaseFilmInfoByIdAsync(Guid id, bool trackCahnges)
         {
-            return await GetByConditionAsync(b => b.Id.Equals(id), trackCahnges).SingleOrDefaultAsync();
+            return await GetByConditionAsync(b => b.Id.Equals(id), trackCahnges).IncludeAllRelatedData().SingleOrDefaultAsync();
         }
 
-        public async Task<BaseFilmInfo> GetBaseFilmInfoByTitleAndReleaseDate(string title, DateOnly releaseDate, bool trackChanges)
+        public async Task<PagedList<BaseFilmInfo>> GetFilteredBaseFilmInfosAsync(FilmParameters filmParameters, bool trackChanges)
+        {
+            var filteredList = await GetAllAsync(trackChanges)
+                         .IncludeAllRelatedData()
+                         .Search(filmParameters.SearchTerm)
+                         .FilterFilmInfosByReleaseYear(filmParameters.MinReleaseYear, filmParameters.MaxReleaseYear)
+                         .GenresContains(filmParameters.GenreId)
+                         .CountriesContains(filmParameters.CountryId)
+                         .CollectionContains(filmParameters.CollectionId)
+                         .Sort(filmParameters.OrderBy)
+                         .ToListAsync();
+
+            return PagedList<BaseFilmInfo>.ToPagedList(filteredList, filmParameters.PageNumber, filmParameters.PageSize);
+        }
+
+        public async Task<BaseFilmInfo> GetByIdWithAllRelatedDataAsync(Guid id, bool trackChanges)
+        {
+            return await GetByConditionAsync(b => b.Id.Equals(id), trackChanges).IncludeAllRelatedData().SingleOrDefaultAsync();
+        }
+
+        public async Task<BaseFilmInfo> GetBaseFilmInfoByTitleAndReleaseDateAsync(string title, DateOnly releaseDate, bool trackChanges)
         {
             return await GetByConditionAsync(b => b.ReleaseDate == releaseDate && b.Title == title, trackChanges).FirstOrDefaultAsync();
         }
