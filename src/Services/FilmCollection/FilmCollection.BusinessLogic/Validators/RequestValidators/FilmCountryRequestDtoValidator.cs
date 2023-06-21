370b35dccb2bce2897172d3fa5b537fc16c2fd4e
labstudent <padalitsa2055@gmail.com>
2023-06-21 09:58:06 +0300

    Delete unnessary validation rules.

--- src/Services/FilmCollection/FilmCollection.BusinessLogic/Validators/RequestValidators/FilmCountryRequestDtoValidator.cs
+++ src/Services/FilmCollection/FilmCollection.BusinessLogic/Validators/RequestValidators/FilmCountryRequestDtoValidator.cs
@@ -8,13 +8,10 @@ namespace FilmCollection.BusinessLogic.Validators.RequestValidators
         public FilmCountryRequestDtoValidator()
         {
             RuleFor(fc => fc.CountryId)
-                .NotNull()
-                .WithMessage("Country id is required")
                 .IsInEnum()
                 .WithMessage("Invalid country");
 
             RuleFor(fc => fc.BaseFilmInfoId)
-                .NotNull()
                 .NotEmpty()
                 .WithMessage("Film id is required");
         }
