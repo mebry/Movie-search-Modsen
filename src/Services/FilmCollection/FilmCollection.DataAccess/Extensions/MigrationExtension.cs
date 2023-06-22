867a07395c3a25ece9f502cc5c7bf6e0b9794196
labstudent <padalitsa2055@gmail.com>
2023-06-23 01:06:18 +0300

    Migration call added.

---
+++ src/Services/FilmCollection/FilmCollection.DataAccess/Extensions/MigrationExtension.cs
@@ -0,0 +1,23 @@
+﻿using FilmCollection.DataAccess.Contexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmCollection.DataAccess.Extensions
+{
+    public static class MigrationExtension
+    {
+        public static async Task ApplyMigration(this IApplicationBuilder applicationBuilder)
+        {
+            using var scope = applicationBuilder.ApplicationServices.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetService<FilmCollectionContext>();
+            await dbContext?.Database.MigrateAsync();
+        }
+    }
+}
