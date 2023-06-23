6be784924bd9360b97084f2756d87b500fd90b8b
mebry <vadushko@gmail.com>
2023-06-23 12:53:46 +0300

    Update swagger

--- src/Services/Film/Film.API/Program.cs
+++ src/Services/Film/Film.API/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
