c8f93802e1c21824c881100eabdd0d3eda787188
Osten Power <osten_power@mail.ru>
2023-06-23 12:30:46 +0300

    add cosumers

---
+++ src/Services/Rating/Rating.BusinessLogic/MassTransit/Consumers/CreateFilmMessageConsumer.cs
@@ -0,0 +1,27 @@
+﻿using Mapster;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Rating.BusinessLogic.DTOs;
+using Rating.BusinessLogic.Services.FilmServices;
+using Shared.Messages.FilmMessages;
+
+namespace Rating.BusinessLogic.MassTransit.Consumers
+{
+    public class CreateFilmMessageConsumer : IConsumer<CreatedFilmMessage>
+    {
+        private readonly IFilmService _filmService;
+        private readonly ILogger<CreateFilmMessageConsumer> _logger;
+
+        public CreateFilmMessageConsumer(IFilmService filmService, ILogger<CreateFilmMessageConsumer> logger)
+        {
+            _filmService = filmService;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<CreatedFilmMessage> context)
+        {
+            await _filmService.CreateAsync(context.Message.Id);
+            _logger.LogInformation("Film was created");
+        }
+    }
+}
