a59168bd176e466063a21b6a45ddf84f6303af33
mebry <vadushko@gmail.com>
2023-06-23 12:11:46 +0300

    Add consumers and related models. Add an IPublishEndpoint interface into a FilmServise

---
+++ src/Services/Film/Film.BusinessLogic/MassTransit/Consumers/UpdateAverageRatingMessageConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Film.DataAccess.Repositories.Implementations;
+using Film.DataAccess.Repositories.Interfaces;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared.Messages;
+
+namespace Film.BusinessLogic.MassTransit.Consumers
+{
+    public class UpdateAverageRatingMessageConsumer : IConsumer<UpdateAverageRatingMessage>
+    {
+        private readonly IFilmRepository _filmRepository;
+        private readonly ILogger<UpdateAverageRatingMessageConsumer> _logger;
+
+        public UpdateAverageRatingMessageConsumer(IFilmRepository filmRepository, ILogger<UpdateAverageRatingMessageConsumer> logger)
+        {
+            _filmRepository = filmRepository;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<UpdateAverageRatingMessage> context)
+        {
+            var foundFilm = await _filmRepository.GetByIdAsync(context.Message.FilmId);
+            
+            foundFilm.AverageRating = context.Message.AverageRating;
+
+            _filmRepository.Update(foundFilm);
+            await _filmRepository.SaveChangesAsync();
+        }
+    }
+}
