7084ce30f5e5ba6dccff8c439b8cc34cf3e6ff71
Osten Power <osten_power@mail.ru>
2023-06-24 16:48:21 +0300

    add User entity

--- src/Services/Reporting/Reporting.DataAccess/Entities/Rating.cs
+++ src/Services/Reporting/Reporting.DataAccess/Entities/Rating.cs
@@ -3,7 +3,8 @@
     public class Rating
     {
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
         public Guid? FilmId { get; set; }
         public Film? Film { get; set; }
         public int Score { get; set; }
