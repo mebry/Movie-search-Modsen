4df68b24182536536bcf61d5a0be080305668c68
Osten Power <osten_power@mail.ru>
2023-06-26 12:16:35 +0300

    add extensions

---
+++ src/Services/Reporting/Reporting.DataAccess/Extensions/DatabaseConfigurationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Reporting.DataAccess.Contexts;
+
+namespace Reporting.DataAccess.Extensions
+{
+    public static class DatabaseConfigurationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using var services = app.ApplicationServices.CreateScope();
+
+            var dbContext = services.ServiceProvider.GetService<ReportingContext>();
+            dbContext?.Database.Migrate();
+        }
+    }
+}
