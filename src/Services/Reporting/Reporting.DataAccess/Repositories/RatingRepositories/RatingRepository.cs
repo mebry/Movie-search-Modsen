689dd2f14c9e1e6883f80bcc49022a9a1bb0615e
Osten Power <osten_power@mail.ru>
2023-06-26 12:24:09 +0300

    add ratingrepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/RatingRepositories/RatingRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.RatingRepositories
+{
+    internal class RatingRepository : EFRepository<Rating>, IRatingRepository
+    {
+        public RatingRepository(ReportingContext context) : base(context)
+        {
+        }
+
+        public override async Task<Rating?> GetByIdAsync(Guid id)
+             => await _context.Ratings
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
