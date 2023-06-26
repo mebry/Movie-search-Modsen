8515adba4c60fd9267cf27feefeb24a5892a2fdf
Osten Power <osten_power@mail.ru>
2023-06-26 12:24:01 +0300

    add positionrepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/PositionRepositories/IPostionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Reporting.DataAccess.Entities;
+using Reporting.DataAccess.Interfaces;
+
+namespace Reporting.DataAccess.Repositories.PositionRepositories
+{
+    public interface IPostionRepository : IRepository<Position>
+    {
+    }
+}
---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/PositionRepositories/PositionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.PositionRepositories
+{
+    internal class PositionRepository : EFRepository<Position>, IPostionRepository
+    {
+        public PositionRepository(ReportingContext context) : base(context)
+        {
+        }
+
+        public override async Task<Position?> GetByIdAsync(Guid id)
+             => await _context.Positions
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
