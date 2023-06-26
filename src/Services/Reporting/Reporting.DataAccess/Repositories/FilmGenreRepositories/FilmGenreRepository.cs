d2eea1229c739ba79e6dc95a752e9de45a77dbde
Osten Power <osten_power@mail.ru>
2023-06-26 16:42:05 +0300

    add filmgenrerepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/FilmGenreRepositories/FilmGenreRepository.cs
@@ -0,0 +1,32 @@
+﻿using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.FilmGenreRepositories
+{
+    public class FilmGenreRepository : IFilmGenreRepository
+    {
+        private readonly ReportingContext _context;
+
+        public FilmGenreRepository(ReportingContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(FilmGenre filmGenre)
+            => _context.FilmGenres.Add(filmGenre);
+
+        public void Delete(Guid filmId, Guid genreId)
+        {
+            var filmGenre = new FilmGenre
+            {
+                FilmId = filmId,
+                GenreId = genreId
+            };
+
+            _context.FilmGenres.Remove(filmGenre);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/FilmGenreRepositories/IFilmGenreRepository.cs
@@ -0,0 +1,11 @@
+﻿using Reporting.DataAccess.Entities;
+using Reporting.DataAccess.Interfaces;
+
+namespace Reporting.DataAccess.Repositories.FilmGenreRepositories
+{
+    public interface IFilmGenreRepository : ISaveChangesAsync
+    {
+        public void Create(FilmGenre filmGenre);
+        public void Delete(Guid filmId, Guid genreId);
+    }
+}
