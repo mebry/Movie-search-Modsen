adb328c143c9ba4117e8d9de6f860b147bfcc11e
Osten Power <osten_power@mail.ru>
2023-06-26 12:16:10 +0300

    create EFRepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/EFRepository.cs
@@ -0,0 +1,32 @@
+﻿using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Interfaces;
+
+namespace Reporting.DataAccess.Repositories
+{
+    internal abstract class EFRepository<T> : IRepository<T> where T : class
+    {
+        private readonly ReportingContext _context;
+
+        protected EFRepository(ReportingContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T entity)
+            => _context.Add(entity);
+
+        public void Update(T entity)
+             => _context.Update(entity);
+
+        public void Delete(Guid id)
+        {
+            var obj = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(obj!);
+        }
+
+        public abstract Task<T> GetByIdAsync(Guid id);
+
+        public async Task SaveChangesAsync()
+             => await _context.SaveChangesAsync();
+    }
+}
