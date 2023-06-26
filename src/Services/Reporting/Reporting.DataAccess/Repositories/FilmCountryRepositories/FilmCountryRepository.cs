e00f52444b432de9dfee788cc10a82ee398bb7a3
Osten Power <osten_power@mail.ru>
2023-06-26 16:41:53 +0300

    add filmcountryrepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/FilmCountryRepositories/FilmCountryRepository.cs
@@ -0,0 +1,38 @@
+﻿using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+using Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reporting.DataAccess.Repositories.FilmCountryRepositories
+{
+    internal class FilmCountryRepository : IFilmCountryRepository
+    {
+        private readonly ReportingContext _context;
+
+        public FilmCountryRepository(ReportingContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(FilmCountry filmCountry)
+            => _context.FilmCountries.Add(filmCountry);
+
+
+        public void Delete(Guid filmId, Countries countryEnum)
+        {
+            var filmCountry = _context.FilmCountries
+                .FirstOrDefault(fc => (fc.FilmId == filmId)
+                && (fc.CountryId == countryEnum));
+
+            if(filmCountry != null)
+                _context.FilmCountries.Remove(filmCountry);
+        }
+
+        public async Task SaveChangesAsync()
+            => await _context.SaveChangesAsync();
+    }
+}
---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/FilmCountryRepositories/IFilmCountryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Reporting.DataAccess.Entities;
+using Reporting.DataAccess.Interfaces;
+using Shared.Enums;
+
+namespace Reporting.DataAccess.Repositories.FilmCountryRepositories
+{
+    public interface IFilmCountryRepository : ISaveChangesAsync
+    {
+        public void Create(FilmCountry filmCountry);
+        public void Delete(Guid filmId, Countries countryEnum);
+    }
+}
