b73d186af89c269835cb6f172cf2017e9f1c68e3
Osten Power <osten_power@mail.ru>
2023-06-26 16:05:06 +0300

    setting consumer

---
+++ src/Services/Rating/Rating.BusinessLogic/MassTransit/Consumers/DeleteFilmMessageConsimer.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Rating.DataAccess.Entities;
+using Rating.DataAccess.Repositories.FilmRepositories;
+using Shared.Messages.FilmMessages;
+
+namespace Rating.BusinessLogic.MassTransit.Consumers
+{
+    public class DeleteFilmMessageConsimer : IConsumer<RemovedFilmMessage>
+    {
+        private readonly IFilmRepository _filmRepository;
+        private readonly ILogger<CreateFilmMessageConsumer> _logger;
+
+        public DeleteFilmMessageConsimer(IFilmRepository filmRepository, ILogger<CreateFilmMessageConsumer> logger)
+        {
+            _filmRepository = filmRepository;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<RemovedFilmMessage> context)
+        {
+            var film = context.Adapt<Film>();
+
+            _filmRepository.Delete(film);
+
+            await _filmRepository.SaveAsync();
+
+            _logger.LogInformation("Film was deleted");
+        }
+    }
+}
--- src/Services/Rating/Rating.WebAPI/Extensions/MassTransitExtensions.cs
+++ src/Services/Rating/Rating.WebAPI/Extensions/MassTransitExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using MassTransit;
 using Rating.BusinessLogic.MassTransit.Consumers;
 using Rating.DataAccess.Contexts;
+using Shared.Messages.FilmMessages;
 using Shared.Messages.RatingMessages;
 using System.Reflection;
 
@@ -39,8 +40,20 @@ namespace Rating.WebAPI.Extensions
 
                     cfg.ReceiveEndpoint(config["RabbitMQ:ReceiveEndpoints:FilmCreate"]!, x =>
                     {
+                        x.Bind<CreatedFilmMessage>();
+                        x.ConfigureConsumer<CreateFilmMessageConsumer>(context);
+                    });
+
+                    cfg.ReceiveEndpoint(config["RabbitMQ:ReceiveEndpoints:FilmUpdate"]!, x =>
+                    {
                         x.Bind<UpdateAverageRatingMessage>();
                         x.ConfigureConsumer<CreateFilmMessageConsumer>(context);
+                    });
+
+                    cfg.ReceiveEndpoint(config["RabbitMQ:ReceiveEndpoints:FilmDelete"]!, x =>
+                    {
+                        x.Bind<RemovedFilmMessage>();
+                        x.ConfigureConsumer<DeleteFilmMessageConsimer>(context);
                     });
                 });
             });
