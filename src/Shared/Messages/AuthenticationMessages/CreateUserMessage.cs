d7d2c779ab9a00f0f05fe8b31981d2d938bb5634
labstudent <padalitsa2055@gmail.com>
2023-06-26 11:50:07 +0300

    User messages added.

---
+++ src/Shared/Messages/AuthenticationMessages/CreateUserMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Messages.AuthenticationMessages
+{
+    public class CreateUserMessage
+    {
+        public Guid Id { get; set; }    
+        public string Username { get; set; }
+    }
+}
