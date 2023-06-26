e12f5525b7aa803c7258a39e3948fb766fa14d47
Osten Power <osten_power@mail.ru>
2023-06-26 16:42:19 +0300

    add staffpersonrepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/StaffPersonPositironRepositories/ISatffPersonPositionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Reporting.DataAccess.Entities;
+using Reporting.DataAccess.Interfaces;
+
+namespace Reporting.DataAccess.Repositories.StaffPersonPositironRepositories
+{
+    public interface ISatffPersonPositionRepository : ISaveChangesAsync
+    {
+        public void Create(StaffPersonPosition staffPerson);
+        public void Delete(Guid filmId, Guid staffPersonId, Guid positionId);
+    }
+}
