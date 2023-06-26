7e03b046aefe147948994da334385469acc2351e
Osten Power <osten_power@mail.ru>
2023-06-26 12:21:39 +0300

    add FilmRepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/FilmRepositories/FilmRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.FilmRepositories
+{
+    internal class FilmRepository : EFRepository<Film>, IFilmRepository
+    {
+        public FilmRepository(ReportingContext context) : base(context)
+        {
+        }
+
+        public override async Task<Film?> GetByIdAsync(Guid id)
+              => await _context.Films
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/FilmRepositories/IFilmRepository.cs
@@ -0,0 +1,9 @@
+﻿using Reporting.DataAccess.Entities;
+using Reporting.DataAccess.Interfaces;
+
+namespace Reporting.DataAccess.Repositories.FilmRepositories
+{
+    public interface IFilmRepository : IRepository<Film>
+    {
+    }
+}
