7de4930e1219882e84d6f25388254f8c9badc4d8
Osten Power <osten_power@mail.ru>
2023-06-26 12:28:33 +0300

    add userrepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/UserRepositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.UserRepository
+{
+    internal class UserRepository : EFRepository<User>, IUserRepository
+    {
+        public UserRepository(ReportingContext context) : base(context)
+        {
+        }
+
+        public override async Task<User?> GetByIdAsync(Guid id)
+              => await _context.Users
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
+}
