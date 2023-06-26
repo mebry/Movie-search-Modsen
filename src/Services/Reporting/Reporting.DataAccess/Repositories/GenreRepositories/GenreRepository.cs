5a11f7c91cb39ea95e371339c12051775b18ace4
Osten Power <osten_power@mail.ru>
2023-06-26 12:23:49 +0300

    add genrerepotiry

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/GenreRepositories/GenreRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.GenreRepositories
+{
+    internal class GenreRepository : EFRepository<Genre>, IGenreRepository
+    {
+        public GenreRepository(ReportingContext context) : base(context)
+        {
+        }
+
+        public override async Task<Genre?> GetByIdAsync(Guid id)
+             => await _context.Genres
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
