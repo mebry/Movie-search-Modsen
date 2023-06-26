fb4bbbaa830683642b0740f67599524cf69cd2e1
Osten Power <osten_power@mail.ru>
2023-06-26 16:57:30 +0300

    move applymigration to shared

--- src/Shared/Extensions/DatabaseConfigurationExtensions.cs
+++ src/Shared/Extensions/DatabaseConfigurationExtensions.cs
@@ -1,17 +1,17 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Reporting.DataAccess.Contexts;
 
 namespace Reporting.DataAccess.Extensions
 {
     public static class DatabaseConfigurationExtensions
     {
-        public static void ApplyMigrations(this IApplicationBuilder app)
+        public static void ApplyMigrations<TDbContext>(this IApplicationBuilder app)
+            where TDbContext : DbContext
         {
             using var services = app.ApplicationServices.CreateScope();
 
-            var dbContext = services.ServiceProvider.GetService<ReportingContext>();
+            var dbContext = services.ServiceProvider.GetService<TDbContext>();
             dbContext?.Database.Migrate();
         }
     }
