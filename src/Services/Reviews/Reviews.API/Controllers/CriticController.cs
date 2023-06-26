4d5072f65b364b9f88b6809415ede3b9e4c58321
Elizaveta Zhelihovskaya <elizaveta132003@gmail.com>
2023-06-26 17:01:46 +0300

    refactoring controllers

--- src/Services/Reviews/Reviews.API/Controllers/CriticController.cs
+++ src/Services/Reviews/Reviews.API/Controllers/CriticController.cs
@@ -21,6 +21,9 @@ namespace Reviews.API.Controllers
         /// <param name="requestCriticDTO">The critic data to create.</param>
         /// <returns>Returns the created critic.</returns>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public async Task<ActionResult<ResponseCriticDTO>> CreateAsync(RequestCriticDTO requestCriticDTO)
         {
             var result = await _criticService.CreateAsync(requestCriticDTO);
@@ -34,6 +37,8 @@ namespace Reviews.API.Controllers
         /// <param name="id">The ID of the critic.</param>
         /// <returns>Returns the critic with the specified ID.</returns>
         [HttpGet("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ResponseCriticDTO>> GetByIdAsync(Guid id)
         {
             var result = await _criticService.GetByIdAsync(id);
@@ -47,6 +52,8 @@ namespace Reviews.API.Controllers
         /// <param name="id">The ID of the critic to remove.</param>
         /// <returns>Returns the removed critic.</returns>
         [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ResponseCriticDTO>> RemoveAsync(Guid id)
         {
             var result = await _criticService.RemoveCriticAsync(id);
@@ -61,6 +68,9 @@ namespace Reviews.API.Controllers
         /// <param name="requestCriticDTO">The updated critic data.</param>
         /// <returns>Returns the updated critic.</returns>
         [HttpPut("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ResponseCriticDTO>> UpdateAsync(Guid id, RequestCriticDTO requestCriticDTO)
         {
             var result = await _criticService.UpdateAsync(id, requestCriticDTO);
