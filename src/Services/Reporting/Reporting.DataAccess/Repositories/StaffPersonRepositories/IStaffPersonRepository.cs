9d3221519d216d975d5f4c39c6cd0e7ebc488022
Osten Power <osten_power@mail.ru>
2023-06-26 12:28:25 +0300

    add staffpersonrepository

---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/StaffPersonRepositories/IStaffPersonRepository.cs
@@ -0,0 +1,9 @@
+﻿using Reporting.DataAccess.Entities;
+using Reporting.DataAccess.Interfaces;
+
+namespace Reporting.DataAccess.Repositories.StaffPersonRepositories
+{
+    public interface IStaffPersonRepository : IRepository<StaffPerson>
+    {
+    }
+}
---
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/StaffPersonRepositories/StaffPersonRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
+using Reporting.DataAccess.Entities;
+
+namespace Reporting.DataAccess.Repositories.StaffPersonRepositories
+{
+    internal class StaffPersonRepository : EFRepository<StaffPerson>, IStaffPersonRepository
+    {
+        public StaffPersonRepository(ReportingContext context) : base(context)
+        {
+        }
+
+        public override async Task<StaffPerson?> GetByIdAsync(Guid id)
+              => await _context.StaffPeople
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.Id == id);
+    }
+}
