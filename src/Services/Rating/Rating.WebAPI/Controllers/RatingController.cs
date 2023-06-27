b145584fb982e043c89d5e671b3c9fab93d9c424
Osten Power <osten_power@mail.ru>
2023-06-27 11:16:21 +0300

    add ProducesResponseType and xml comments

--- src/Services/Rating/Rating.WebAPI/Controllers/RatingController.cs
+++ src/Services/Rating/Rating.WebAPI/Controllers/RatingController.cs
@@ -15,7 +15,15 @@ namespace Rating.API.Controllers
             _ratingService = ratingService;
         }
 
+        /// <summary>
+        /// Retrieves a rating by Id.
+        /// </summary>
+        /// <param name="id">The Id of the rating.</param>
+        /// <response code="200">Return response rating.</response>
+        /// <response code="404">If id is not found.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ResponseRatingDTO>> GetById(Guid id)
         {
             var rating = await _ratingService.GetByIdAsync(id);
@@ -23,7 +31,14 @@ namespace Rating.API.Controllers
             return Ok(rating);
         }
 
+        /// <summary>
+        /// Create rating
+        /// </summary>
+        /// <response code="200">Return response rating.</response>
+        /// <response code="409">If the user has already rated the film with filmId.</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         public async Task<ActionResult<ResponseRatingDTO>> Create(RequestRatingDTO ratingDto)
         {
             var rating = await _ratingService.CreateAsync(ratingDto);
@@ -31,7 +46,15 @@ namespace Rating.API.Controllers
             return Ok(rating);
         }
 
+        /// <summary>
+        /// Update rating.
+        /// </summary>
+        /// <param name="id">The Id of the rating.</param>
+        /// <response code="200">Return response rating.</response>
+        /// <response code="404">If id is not found.</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ResponseRatingDTO>> Update(Guid id, RequestRatingDTO ratingDto)
         {
             var rating = await _ratingService.UpdateAsync(id, ratingDto);
@@ -39,7 +62,15 @@ namespace Rating.API.Controllers
             return Ok(rating);
         }
 
+        /// <summary>
+        /// Delete rating.
+        /// </summary>
+        /// <param name="id">The Id of the rating.</param>
+        /// <response code="200">Return response rating.</response>
+        /// <response code="404">If id is not found.</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ResponseRatingDTO>> Delete(Guid id)
         {
             var rating = await _ratingService.DeleteAsync(id);
