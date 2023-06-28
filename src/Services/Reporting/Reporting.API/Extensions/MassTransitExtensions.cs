1f53dd372866a07d3fed5f7889aec7bde600bb5c
Osten Power <osten_power@mail.ru>
2023-06-28 11:09:12 +0300

    add config

---
+++ src/Services/Reporting/Reporting.API/Extensions/MassTransitExtensions.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using Reporting.DataAccess.Contexts;
+using System.Reflection;
+
+namespace Reporting.API.Extensions
+{
+    public static class MassTransitExtensions
+    {
+        public static void ConfigureMassTransit(this IServiceCollection services, ConfigurationManager config)
+        {
+            services.AddMassTransit(x =>
+            {
+
+                var assembly = Assembly.LoadFrom(config["Path"]!);
+                var host = config["RabbitMQ:Host"];
+                var virtualHost = config["RabbitMQ:VirtualHost"];
+                var username = config["RabbitMQ:Username"];
+                var password = config["RabbitMQ:Password"];
+
+                x.AddEntityFrameworkOutbox<ReportingContext>(o =>
+                {
+                    o.UseSqlServer();
+
+                    o.QueryDelay = TimeSpan.FromSeconds(1);
+
+                    o.UseBusOutbox();
+                });
+
+                x.AddConsumers(assembly);
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(host, virtualHost, h =>
+                    {
+                        h.Username(username);
+                        h.Password(password);
+                    });
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+        }
+    }
+}
