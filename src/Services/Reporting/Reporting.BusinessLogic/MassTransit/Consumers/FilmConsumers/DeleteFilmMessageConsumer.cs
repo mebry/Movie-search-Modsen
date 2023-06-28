ba81684b9f0ac035fd8d4edd0de6974adb375b49
Osten Power <osten_power@mail.ru>
2023-06-28 09:58:18 +0300

    add film consumers

---
+++ src/Services/Reporting/Reporting.BusinessLogic/MassTransit/Consumers/FilmConsumers/DeleteFilmMessageConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Reporting.BusinessLogic.Services.FilmServices;
+using Shared.Messages.FilmMessages;
+
+namespace Reporting.BusinessLogic.MassTransit.Consumers.FilmConsumers
+{
+    internal class DeleteFilmMessageConsumer : IConsumer<RemovedFilmMessage>
+    {
+        private readonly IFilmDataCaptureService _filmService;
+        private readonly ILogger<CreateFilmMessageConsumer> _logger;
+
+        public DeleteFilmMessageConsumer(IFilmDataCaptureService filmService, ILogger<CreateFilmMessageConsumer> logger)
+        {
+            _filmService = filmService;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<RemovedFilmMessage> context)
+        {
+            Guid filmId = context.Message.Id;
+
+            await _filmService.DeleteAsync(filmId);
+
+            _logger.LogInformation("Film was created");
+        }
+    }
+}
---
+++ src/Services/Reporting/Reporting.BusinessLogic/MassTransit/Consumers/FilmConsumers/UpdateFilmMessageConsumer.cs
@@ -0,0 +1,30 @@
+﻿using Mapster;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Reporting.BusinessLogic.DTOs.ConsumerDTOs;
+using Reporting.BusinessLogic.Services.FilmServices;
+using Shared.Messages.FilmMessages;
+
+namespace Reporting.BusinessLogic.MassTransit.Consumers.FilmConsumers
+{
+    internal class UpdateFilmMessageConsumer : IConsumer<UpdatedFilmMessage>
+    {
+        private readonly IFilmDataCaptureService _filmService;
+        private readonly ILogger<CreateFilmMessageConsumer> _logger;
+
+        public UpdateFilmMessageConsumer(IFilmDataCaptureService filmService, ILogger<CreateFilmMessageConsumer> logger)
+        {
+            _filmService = filmService;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<UpdatedFilmMessage> context)
+        {
+            var filmConsumer = context.Message.Adapt<ConsumerFilmDTO>();
+
+            await _filmService.UpdateAsync(filmConsumer);
+
+            _logger.LogInformation("Film was created");
+        }
+    }
+}
