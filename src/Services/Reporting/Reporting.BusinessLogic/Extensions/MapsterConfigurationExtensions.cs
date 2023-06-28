9f7ffdf1130156457872254592a1d42221d84147
Osten Power <osten_power@mail.ru>
2023-06-28 10:44:06 +0300

    add mapster settings

---
+++ src/Services/Reporting/Reporting.BusinessLogic/Extensions/MapsterConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+using Reporting.BusinessLogic.DTOs.ConsumerDTOs;
+using Reporting.DataAccess.Entities;
+using Shared.Messages.RatingMessages;
+
+namespace Reporting.BusinessLogic.Extensions
+{
+    internal static class MapsterConfigurationExtensions
+    {
+        public static void RegisterMapsterConfiguration(this IServiceCollection services)
+        {
+            TypeAdapterConfig<Film, ConsumerAverageRatingDTO>
+            .NewConfig()
+            .Map(dest => dest.FilmId, src => src.Id);
+
+            TypeAdapterConfig<Film, ConsumerCountOfScoresDTO>
+            .NewConfig()
+            .Map(dest => dest.FilmId, src => src.Id);
+        }
+    }
+}
