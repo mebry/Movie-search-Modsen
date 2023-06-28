d7c38ea821fc024441aa3b799b4da81316d986c5
Osten Power <osten_power@mail.ru>
2023-06-28 10:15:25 +0300

    add genre consumer

---
+++ src/Services/Reporting/Reporting.BusinessLogic/MassTransit/Consumers/GenreConsumers/RemovedGenreMessageConsumer.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Reporting.BusinessLogic.MassTransit.Consumers.FilmConsumers;
+using Reporting.BusinessLogic.Services.GenreServices;
+using Shared.Messages.GenreMessages;
+
+namespace Reporting.BusinessLogic.MassTransit.Consumers.GenreConsumers
+{
+    internal class RemovedGenreMessageConsumer : IConsumer<RemovedGenreMessage>
+    {
+        private readonly IGenreDataCaptureService _genreDataCaptureService;
+        private readonly ILogger<CreatedFilmMessageConsumer> _logger;
+
+        public RemovedGenreMessageConsumer(IGenreDataCaptureService genreDataCaptureService,
+            ILogger<CreatedFilmMessageConsumer> logger)
+        {
+            _genreDataCaptureService = genreDataCaptureService;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<RemovedGenreMessage> context)
+        {
+            var message = context.Message;
+            var genreId = message.Id;
+
+            await _genreDataCaptureService.DeleteAsync(genreId);
+
+            _logger.LogInformation("Genre was removed");
+        }
+    }
+}
---
+++ src/Services/Reporting/Reporting.BusinessLogic/MassTransit/Consumers/GenreConsumers/UpdatedGenreMessageConsumer.cs
@@ -0,0 +1,33 @@
+﻿using Mapster;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Reporting.BusinessLogic.DTOs.ConsumerDTOs;
+using Reporting.BusinessLogic.MassTransit.Consumers.FilmConsumers;
+using Reporting.BusinessLogic.Services.GenreServices;
+using Shared.Messages.GenreMessages;
+
+namespace Reporting.BusinessLogic.MassTransit.Consumers.GenreConsumers
+{
+    internal class UpdatedGenreMessageConsumer : IConsumer<UpdatedGenreMessage>
+    {
+        private readonly IGenreDataCaptureService _genreDataCaptureService;
+        private readonly ILogger<CreatedFilmMessageConsumer> _logger;
+
+        public UpdatedGenreMessageConsumer(IGenreDataCaptureService genreDataCaptureService,
+            ILogger<CreatedFilmMessageConsumer> logger)
+        {
+            _genreDataCaptureService = genreDataCaptureService;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<UpdatedGenreMessage> context)
+        {
+            var message = context.Message;
+            var genreConsumer = message.Adapt<ConsumerGenreDTO>();
+
+            await _genreDataCaptureService.UpdateAsync(genreConsumer);
+
+            _logger.LogInformation("Genre was updated");
+        }
+    }
+}
