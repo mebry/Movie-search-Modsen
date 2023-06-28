6ffc2a23a8f07cd9ace8737fc56ea8026a634a07
Osten Power <osten_power@mail.ru>
2023-06-28 10:48:04 +0300

    add staff person position consumer

---
+++ src/Services/Reporting/Reporting.BusinessLogic/MassTransit/Consumers/StaffPersonPositionConsumers/CreatedStaffPersonPositionMessageConsumer.cs
@@ -0,0 +1,33 @@
+﻿using Mapster;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+using Reporting.BusinessLogic.DTOs.ConsumerDTOs;
+using Reporting.BusinessLogic.MassTransit.Consumers.StaffPersonConsumers;
+using Reporting.BusinessLogic.Services.StaffPersonPositionServices;
+using Shared.Messages.StaffMessages;
+
+namespace Reporting.BusinessLogic.MassTransit.Consumers.StaffPersonPositionConsumers
+{
+    internal class CreatedStaffPersonPositionMessageConsumer : IConsumer<CreatedStaffPersonPositionMessage>
+    {
+        private readonly IStaffPersonPositionDataCaptureService _staffPersonPositionDataCaptureService;
+        private readonly ILogger<CreatedStaffPersonMessageConsumer> _logger;
+
+        public CreatedStaffPersonPositionMessageConsumer(IStaffPersonPositionDataCaptureService staffPersonPositionDataCaptureService,
+            ILogger<CreatedStaffPersonMessageConsumer> logger)
+        {
+            _staffPersonPositionDataCaptureService = staffPersonPositionDataCaptureService;
+            _logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<CreatedStaffPersonPositionMessage> context)
+        {
+            var message = context.Message;
+            var staffPersonComsumer = message.Adapt<ConsumerStaffPersonPositionDTO>();
+
+            await _staffPersonPositionDataCaptureService.CreateAsync(staffPersonComsumer);
+
+            _logger.LogInformation("Staff person position was created");
+        }
+    }
+}
