74d3f2caf7a6d57894104df378a9738dc1655eb1
labstudent <padalitsa2055@gmail.com>
2023-06-28 15:04:57 +0300

    Static data to seed added.

---
+++ src/Services/Authentication/DataAccess/Seed/DataToSeed.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Seed
+{
+    public static class DataToSeed
+    {
+        public static User GetAdminUserToAdd()
+            =>
+                new User()
+                {
+                    Surname = "Padalitsa",
+                    Name = "Maxim",
+                    UserName = "Admin",
+
+                };
+
+
+        public static string AdminPassword = "qwe123";
+    }
+}
