8078001aa376103922002556adf060a2db3e0afc
labstudent <padalitsa2055@gmail.com>
2023-06-28 14:59:41 +0300

    Roles controller added.

---
+++ src/Services/Authentication/Authentication.API/Controllers/v1/RolesController.cs
@@ -0,0 +1,27 @@
+﻿using Authentication.BusinessLogic.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.API.Controllers.v1
+{
+    [Route("api/{v:apiversion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "v1")]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService) 
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllRolesAsync()
+        {
+            var rolesToReturn = await _roleService.GetAllRolesAsync();
+            return Ok(rolesToReturn);
+        }
+    }
+}
