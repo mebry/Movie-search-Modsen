94b0e702054a8af873e05d061fbf323da42d844e
Osten Power <osten_power@mail.ru>
2023-06-29 17:43:47 +0300

    rename

--- src/Services/Reporting/Reporting.DataAccess/Repositories/StaffPersonPositironRepositories/StaffPersonPositionRepository.cs
+++ src/Services/Reporting/Reporting.DataAccess/Repositories/StaffPersonPositironRepositories/StaffPersonPositionRepository.cs
@@ -1,20 +1,20 @@
-﻿using Reporting.DataAccess.Contexts;
+﻿using Microsoft.EntityFrameworkCore;
+using Reporting.DataAccess.Contexts;
 using Reporting.DataAccess.Entities;
 
 namespace Reporting.DataAccess.Repositories.StaffPersonPositironRepositories
 {
-    internal class SatffPersonPositionRepository : ISatffPersonPositionRepository
+    internal class StaffPersonPositionRepository : IStaffPersonPositionRepository
     {
         private readonly ReportingContext _context;
 
-        public SatffPersonPositionRepository(ReportingContext context)
+        public StaffPersonPositionRepository(ReportingContext context)
         {
             _context = context;
         }
 
         public void Create(StaffPersonPosition entity)
             => _context.StaffPersonPositions.Add(entity);
-
 
         public void Delete(Guid filmId, Guid staffPersonId, Guid positionId)
             => _context.StaffPersonPositions.Remove(
@@ -25,6 +25,13 @@ namespace Reporting.DataAccess.Repositories.StaffPersonPositironRepositories
                     PositionId = positionId
                 });
 
+        public async Task<IEnumerable<StaffPersonPosition>> GetAllByStaffPersonId(Guid staffPersonId)
+            => await _context.StaffPersonPositions
+                .AsNoTracking()
+                .Where(s => s.StaffPersonId == staffPersonId)
+                .Include(s => s.Film)
+                .Include(s => s.Position)
+                .ToListAsync();
 
         public async Task SaveChangesAsync()
             => await _context.SaveChangesAsync();
