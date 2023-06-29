c796b5f6b6690914346a9560f8b864457ae24af7
Osten Power <osten_power@mail.ru>
2023-06-29 17:46:03 +0300

    add staffpersoncontroller

---
+++ src/Services/Reporting/Reporting.BusinessLogic/DTOs/ResponseDTOs/ResponseStaffPersonPositionsInFilms.cs
@@ -0,0 +1,8 @@
+﻿namespace Reporting.BusinessLogic.DTOs.ResponseDTOs
+{
+    public class ResponseStaffPersonPositionsInFilms
+    {
+        public Guid StaffPersonId { get; set; }
+        public Dictionary<ResponsePosition, List<ResponseFilm>> PositionFilms { get; set; } = new();
+    }
+}
--- src/Services/Reporting/Reporting.BusinessLogic/Services/StaffPersonServices/IStaffPersonReportingService.cs
+++ src/Services/Reporting/Reporting.BusinessLogic/Services/StaffPersonServices/IStaffPersonReportingService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Reporting.BusinessLogic.DTOs.ResponseDTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Reporting.BusinessLogic.Services.StaffPersonServices
 {
-    internal interface IStaffPersonReportingService
+    public interface IStaffPersonReportingService
     {
+        public Task<ResponseStaffPersonPositionsInFilms> GetAllPositionsInFIlms(Guid staffPersonId);
     }
 }
