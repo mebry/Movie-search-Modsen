93116f7c38bff50c5e16ae4b6f791f04c0614887
mebry <vadushko@gmail.com>
2023-06-30 01:39:42 +0300

    Add mapping configurations

---
+++ src/Services/Rating/Rating.BusinessLogic/Mappings/CountOfScoresMappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using Mapster;
+using Rating.BusinessLogic.DTOs;
+using Rating.DataAccess.Entities;
+using Shared.Messages.RatingMessages;
+
+namespace Rating.BusinessLogic.Mappings
+{
+    public class CountOfScoresMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            TypeAdapterConfig<FilmDTO, UpdateCountOfScoresMessage>
+            .NewConfig()
+            .Map(dest => dest.FilmId, src => src.Id);
+
+            TypeAdapterConfig<Film, UpdateCountOfScoresMessage>
+            .NewConfig()
+            .Map(dest => dest.FilmId, src => src.Id);
+        }
+    }
+}
