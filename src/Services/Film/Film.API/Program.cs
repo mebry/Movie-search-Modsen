39e7f179f5225b791a6c37c2a42d8c59a3f0e9b5
mebry <vadushko@gmail.com>
2023-06-29 12:45:22 +0300

    Remove unused extensions

--- src/Services/Film/Film.API/Program.cs
+++ src/Services/Film/Film.API/Program.cs
@@ -10,7 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services.ConfigureCors();
 
@@ -28,8 +27,6 @@ if (!app.Environment.IsProduction())
 app.UseHttpsRedirection();
 
 app.UseCors("CorsPolicy");
-
-app.UseAuthorization();
 
 app.MapControllers();
 
