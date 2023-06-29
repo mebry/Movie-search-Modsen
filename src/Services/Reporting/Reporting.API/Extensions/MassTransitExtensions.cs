f3496c4b9f3b840af8d276ecacd652da36bc1d2c
Osten Power <osten_power@mail.ru>
2023-06-29 16:36:34 +0300

    add configs

--- src/Services/Reporting/Reporting.API/Extensions/MassTransitExtensions.cs
+++ src/Services/Reporting/Reporting.API/Extensions/MassTransitExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using Reporting.BusinessLogic.Extensions;
 using Reporting.DataAccess.Contexts;
 using System.Reflection;
 
@@ -11,7 +12,7 @@ namespace Reporting.API.Extensions
             services.AddMassTransit(x =>
             {
 
-                var assembly = Assembly.LoadFrom(config["BllAssemblyPath"]!);
+                var assembly = Assembly.GetAssembly(typeof(ServiceProviderExtensions));
                 var host = config["RabbitMQ:Host"];
                 var virtualHost = config["RabbitMQ:VirtualHost"];
                 var username = config["RabbitMQ:Username"];
--- src/Services/Reporting/Reporting.API/Program.cs
+++ src/Services/Reporting/Reporting.API/Program.cs
@@ -1,8 +1,11 @@
+using Reporting.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureMassTransit(builder.Configuration);
 
 var app = builder.Build();
 
