d8b05567cfed0bdd5a4f1d8cfa96e027feb6f8f0
Osten Power <osten_power@mail.ru>
2023-06-29 20:03:43 +0300

    setting ocelot

--- src/Services/Reporting/Reporting.API/Program.cs
+++ src/Services/Reporting/Reporting.API/Program.cs
@@ -1,10 +1,13 @@
 using Reporting.API.Extensions;
+using Reporting.BusinessLogic.Extensions;
+using Reporting.DataAccess.Contexts;
 using Shared.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddBusinessLogicService();
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureMassTransit(builder.Configuration);
 builder.Services.ConfigureSwagger(builder.Configuration);
@@ -17,7 +20,7 @@ if(app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(s =>
     {
-        s.SwaggerEndpoint("/swagger/swagger.json", "Rating API");
+        s.SwaggerEndpoint("/swagger/v1/swagger.json", "Reporting API");
     });
 }
 
@@ -28,5 +31,7 @@ app.UseCors("CorsPolicy");
 app.ApplyForwardedHeaders();
 
 app.MapControllers();
+
+app.ApplyMigrations<ReportingContext>();
 
 app.Run();
